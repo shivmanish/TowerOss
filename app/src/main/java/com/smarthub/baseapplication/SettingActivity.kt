baad2b3ed1cce48164915ecaa1fd2574decf7d2b
Ankit <maurya@brochill.com>
2022-09-21 08:44:32 +0530

    setting screen completed

--- app/src/main/java/com/smarthub/baseapplication/SettingActivity.kt
+++ app/src/main/java/com/smarthub/baseapplication/SettingActivity.kt
@@ -5,17 +5,18 @@ import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import com.smarthub.baseapplication.databinding.ActivityDashboardBinding
+import com.smarthub.baseapplication.databinding.ActivitySettingBinding
 import com.smarthub.baseapplication.model.LangModel
 
 class SettingActivity : AppCompatActivity() {
 
-    private var dataBinding : ActivityDashboardBinding ?=null
+    private var dataBinding : ActivitySettingBinding ?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        dataBinding = ActivityDashboardBinding.inflate(layoutInflater)
+        dataBinding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(dataBinding?.root)
         initViews()
     }
