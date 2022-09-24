34b2737f6fecaeda437b34d3f60ea4736b13fda5
Ankit <maurya@brochill.com>
2022-09-24 15:11:21 +0530

    profile bug fixes

--- app/src/main/java/com/smarthub/baseapplication/adapter/ProfileListItemAdapter.kt
+++ app/src/main/java/com/smarthub/baseapplication/adapter/ProfileListItemAdapter.kt
@@ -56,24 +56,16 @@ class ProfileListItemAdapter : RecyclerView.Adapter<ProfileListItemAdapter.ViewH
         private fun collapse(v: View) {
             val initialHeight = v.measuredHeight
 
-            val matchParentMeasureSpec =
-                View.MeasureSpec.makeMeasureSpec((v.parent as View).width, View.MeasureSpec.EXACTLY)
-            val wrapContentMeasureSpec =
-                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            val matchParentMeasureSpec = View.MeasureSpec.makeMeasureSpec((v.parent as View).width, View.MeasureSpec.EXACTLY)
+            val wrapContentMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
             v.measure(matchParentMeasureSpec, wrapContentMeasureSpec)
-//        val targetHeight = v.measuredHeight
-            val targetHeight = 0
 
             val a: Animation = object : Animation() {
                 override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-//                v.layoutParams.height =
-//                    if (interpolatedTime == 1f) ViewGroup.LayoutParams.WRAP_CONTENT else (targetHeight * interpolatedTime).toInt()
-//                v.requestLayout()
                     if (interpolatedTime == 1f) {
                         v.visibility = View.GONE
                     } else {
-                        v.layoutParams.height =
-                            initialHeight - (initialHeight * interpolatedTime).toInt()
+                        v.layoutParams.height = initialHeight - (initialHeight * interpolatedTime).toInt()
                         v.requestLayout()
                     }
                 }
@@ -89,20 +81,16 @@ class ProfileListItemAdapter : RecyclerView.Adapter<ProfileListItemAdapter.ViewH
         }
 
         private fun expand(v: View) {
-            val matchParentMeasureSpec =
-                View.MeasureSpec.makeMeasureSpec((v.parent as View).width, View.MeasureSpec.EXACTLY)
-            val wrapContentMeasureSpec =
-                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            val matchParentMeasureSpec = View.MeasureSpec.makeMeasureSpec((v.parent as View).width, View.MeasureSpec.EXACTLY)
+            val wrapContentMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
             v.measure(matchParentMeasureSpec, wrapContentMeasureSpec)
             val targetHeight = v.measuredHeight
 
-            // Older versions of android (pre API 21) cancel animations for views with a height of 0.
             v.layoutParams.height = 1
             val a: Animation = object : Animation() {
                 override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
                     v.visibility = View.VISIBLE
-                    v.layoutParams.height =
-                        if (interpolatedTime == 1f) ViewGroup.LayoutParams.WRAP_CONTENT else (targetHeight * interpolatedTime).toInt()
+                    v.layoutParams.height = if (interpolatedTime == 1f) ViewGroup.LayoutParams.WRAP_CONTENT else (targetHeight * interpolatedTime).toInt()
                     v.requestLayout()
                 }
 
