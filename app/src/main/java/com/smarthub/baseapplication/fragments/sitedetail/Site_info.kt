de1afa3bf9349de1d2ea6fe6c7d62671b8afb696
AnilKumarZ <anilkumarvish21@gmail.com>
2022-09-28 21:10:46 +0530

    updated code till 28sept22

--- app/src/main/java/com/smarthub/baseapplication/fragments/sitedetail/Site_info.kt
+++ app/src/main/java/com/smarthub/baseapplication/fragments/sitedetail/Site_info.kt
@@ -1,20 +1,25 @@
-package com.smarthub.baseapplication.fragments
+package com.smarthub.baseapplication.fragments.sitedetail
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ScrollView
+import androidx.lifecycle.ViewModelProvider
 import com.smarthub.baseapplication.R
+import com.smarthub.baseapplication.ui.site_detail.SiteDetailViewModel
+
 
 private const val ARG_PARAM2 = "param2"
 
 
-class DashboardFragment : Fragment() {
+class Site_info : Fragment() {
     // TODO: Rename and change types of parameters
 //    private var param1: String? = null
     private var param2: String? = null
+    private lateinit var siteDetailViewModel: SiteDetailViewModel
+    private var scrollview: ScrollView?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,16 +35,32 @@ class DashboardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.site_detail_frag, container, false)
-      /*  var textView:TextView=view.findViewById(R.id.text_dashboard)
-        textView.text=param2*/
+        scrollview  = view.findViewById(R.id.scroll_view)
+        siteDetailViewModel = ViewModelProvider(requireActivity())[SiteDetailViewModel::class.java]
+        setScroolListner()
+        /*  var textView:TextView=view.findViewById(R.id.text_dashboard)
+          textView.text=param2*/
         return view
+    }
+    fun setScroolListner(){
+        scrollview?.setOnScrollChangeListener(View.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            val x = scrollY - oldScrollY
+            if (x > 0) {
+                siteDetailViewModel.setScrollViewUp(true)
+            } else if (x < 0) {
+                siteDetailViewModel.setScrollViewUp(false)
+
+            }
+        })
+
+
     }
 
     companion object {
 
         @JvmStatic
         fun newInstance( param2: String) =
-            DashboardFragment().apply {
+            Site_info().apply {
                 arguments = Bundle().apply {
 //                    putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
