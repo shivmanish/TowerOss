b8a4a05a8c91e647186dfa20eb38030c6ce427eb
Ankit <maurya@brochill.com>
2022-09-28 09:42:01 +0530

    changes

--- app/src/main/java/com/smarthub/baseapplication/fragments/DashboardFragment.kt
+++ app/src/main/java/com/smarthub/baseapplication/fragments/DashboardFragment.kt
@@ -1,19 +1,21 @@
 package com.smarthub.baseapplication.fragments
 
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.smarthub.baseapplication.R
+import com.smarthub.baseapplication.databinding.SiteDetailFragBinding
 
 private const val ARG_PARAM2 = "param2"
 
 
 class DashboardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
+    var binding : SiteDetailFragBinding ?=null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,26 +26,26 @@ class DashboardFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.site_detail_frag, container, false)
-      /*  var textView:TextView=view.findViewById(R.id.text_dashboard)
-        textView.text=param2*/
+        binding = SiteDetailFragBinding.bind(view)
         return view
     }
 
-    companion object {
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding?.scrollView?.setOnScrollChangeListener(View.OnScrollChangeListener { view, x, y, oldX, oldY ->
+            Log.d("status", "it works $y $x")
+        })
+    }
+
+    companion object {
         @JvmStatic
-        fun newInstance( param2: String) =
-            DashboardFragment().apply {
-                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        fun newInstance( param2: String) = DashboardFragment().apply {
+                arguments = Bundle().apply { putString(ARG_PARAM2, param2) }
             }
     }
 }
