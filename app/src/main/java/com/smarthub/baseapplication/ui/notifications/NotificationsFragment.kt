d4ae1a6289bec58538ab7353970f9bf338ea40b2
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-10-01 12:52:33 +0530

    notification filter dialog

--- app/src/main/java/com/smarthub/baseapplication/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/notifications/NotificationsFragment.kt
@@ -1,23 +1,30 @@
 package com.smarthub.baseapplication.ui.notifications
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.adapter.NotificationsListAdapter
 import com.smarthub.baseapplication.databinding.FragmentNotificationsBinding
+import java.util.*
+import javax.xml.datatype.DatatypeConstants.MONTHS
 
-import androidx.recyclerview.widget.LinearLayoutManager
 
 class NotificationsFragment : Fragment() {
      private var _binding: FragmentNotificationsBinding? = null
      private lateinit var notificationViewModel:NotificationsViewModel
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+
     private val binding get() = _binding!!
     private var layoutManager: RecyclerView.LayoutManager? = null
 
@@ -32,11 +39,77 @@ class NotificationsFragment : Fragment() {
         return root
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
         super.onViewCreated(itemView, savedInstanceState)
         var layoutManager = LinearLayoutManager(activity)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
-        _binding?.imgMenu.setOnClickListener()
+        _binding?.imgMenu?.setOnClickListener{
+            // on below line we are creating a new bottom sheet dialog.
+            val dialog = BottomSheetDialog(requireActivity())
+            // on below line we are inflating a layout file which we have created.
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog_layout, null)
+            val btnClose = view.findViewById<Button>(R.id.btnReset)
+            val btnFilter = view.findViewById<Button>(R.id.btnFilter)
+            val close = view.findViewById<ImageView>(R.id.imgClose)
+            val startDate = view.findViewById<TextView>(R.id.txtStartDate)
+            val txtEndDate = view.findViewById<TextView>(R.id.txtEndDate)
+            btnClose.setOnClickListener {
+                // on below line we are calling a dismiss
+                // method to close our dialog.
+                dialog.dismiss()
+            }
+            startDate.setOnClickListener {
+                val c: Calendar = Calendar.getInstance()
+                val year: Int = c.get(Calendar.YEAR)
+                val month: Int = c.get(Calendar.MONTH)
+                val day: Int = c.get(Calendar.DAY_OF_MONTH)
+                 val dpd = DatePickerDialog(
+                    requireActivity(),
+                    {
+                            view, year, monthOfYear, dayOfMonth ->
+                        // Display Selected date in textbox
+                        // 12-Apl-2022, DD/MMM/YYYY
+                      //  startDate.setText("" + dayOfMonth + " " + MONTHS.toString().get(monthOfYear) + ", " + year)
+                    }, year, month, day
+                )
+                dpd.show()
+            }
+            txtEndDate.setOnClickListener {
+                val c: Calendar = Calendar.getInstance()
+                val year: Int = c.get(Calendar.YEAR)
+                val month: Int = c.get(Calendar.MONTH)
+                val day: Int = c.get(Calendar.DAY_OF_MONTH)
+                 val dpd = DatePickerDialog(
+                    requireActivity(),
+                    {
+                            view, year, monthOfYear, dayOfMonth ->
+                        // Display Selected date in textbox
+                        txtEndDate.setText("" + dayOfMonth + " " + MONTHS.toString().get(monthOfYear) + ", " + year)
+                    }, year, month, day
+                )
+                dpd.show()
+            }
+            btnFilter.setOnClickListener {
+                // on below line we are calling a dismiss
+                // method to close our dialog.
+                dialog.dismiss()
+            }
+            close.setOnClickListener {
+                // on below line we are calling a dismiss
+                // method to close our dialog.
+                dialog.dismiss()
+            }
+            // below line is use to set cancelable to avoid
+            // closing of dialog box when clicking on the screen.
+            dialog.setCancelable(false)
+            // on below line we are setting
+            // content view to our view.
+            dialog.setContentView(view)
+            // on below line we are calling
+            // a show method to display a dialog.
+            dialog.show()
+        }
         _binding?.rvNotificationList.apply {
             this?.layoutManager = layoutManager
             this?.adapter = NotificationsListAdapter()
@@ -51,7 +124,6 @@ class NotificationsFragment : Fragment() {
 }
 
 private fun ImageView?.setOnClickListener() {
-    print("HH")
     print("HH")
 
 }
