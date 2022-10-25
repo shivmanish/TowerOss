b2249385ca863b1ecb25cccc200da98fad773926
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-10-25 22:40:00 +0530

    SiteLease Layout

--- app/src/main/java/com/smarthub/baseapplication/ui/adapter/AddMoreSiteLeaseAdaptere.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/adapter/AddMoreSiteLeaseAdaptere.kt
@@ -1,7 +1,9 @@
 package com.smarthub.baseapplication.ui.adapter
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.ui.fragments.sitedetail.SiteLeaseF
@@ -17,11 +19,15 @@ class AddMoreSiteLeaseAdaptere(var list: ArrayList<Any>) : RecyclerView.Adapter<
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHold {
         var view  : View = LayoutInflater.from(parent.context).inflate(R.layout.site_lease_tab_layout,parent,false)
 
+        var parentRelative=view.findViewById<RelativeLayout>(R.id.parentRelative)
         return ViewHold(
             view
         )
     }
     override fun onBindViewHolder(holder: ViewHold, position: Int) {
+        holder.itemView.setOnClickListener{
+
+        }
         //btn_next
     }
     override fun getItemCount(): Int {
