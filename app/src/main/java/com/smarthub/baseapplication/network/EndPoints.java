68698a70d1eb34b88c799612d481872fca4347b5
Ankit <maurya@brochill.com>
2022-10-25 20:38:09 +0530

    changes 1

--- app/src/main/java/com/smarthub/baseapplication/network/EndPoints.java
+++ app/src/main/java/com/smarthub/baseapplication/network/EndPoints.java
@@ -2,6 +2,7 @@ package com.smarthub.baseapplication.network;
 
 public class EndPoints {
 
+//    End Points
     public static final String ACCESS_TOKEN =  "api/token/";
     public static final String GET_OTP =  "toweross/Getlogin/";
     public static final String PROFILE = "Touweuserroles/";
