ca241dc35cca51f15839e5b9a6ea8df3b274223a
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-10-26 22:40:42 +0530

    Agreements screens

--- app/src/main/java/com/smarthub/baseapplication/ui/site_lease_acquisition/site_lease_acquisition_adapter.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_lease_acquisition/site_lease_acquisition_adapter.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.smarthub.baseapplication.ui.basic_info.fragment.BasicInfo
+import com.smarthub.baseapplication.ui.site_lease_acquisition.fragment.Agreements
 import com.smarthub.baseapplication.ui.site_lease_acquisition.fragment.Nominals
 import com.smarthub.baseapplication.ui.site_lease_acquisition.fragment.TeamVendor
 
@@ -22,7 +23,7 @@ class SiteLeaseAcquisitionAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm)
                 return TeamVendor()
             }
             2 -> {
-                return BasicInfo()
+                return Agreements()
             }
             3 -> {
                 return BasicInfo()
