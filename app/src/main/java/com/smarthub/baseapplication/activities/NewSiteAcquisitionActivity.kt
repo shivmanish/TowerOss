8f441a4d011eefbac886c5231d3813d06015f31f
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-10-26 20:00:28 +0530

    SiteLease

---
+++ app/src/main/java/com/smarthub/baseapplication/activities/NewSiteAcquisitionActivity.kt
@@ -0,0 +1,12 @@
+package com.smarthub.baseapplication.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smarthub.baseapplication.R
+
+class NewSiteAcquisitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_site_acquisition)
+    }
+}
