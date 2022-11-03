257ac69abd82d51b8593e4613c0c7ad09bb6a835
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-11-03 22:35:04 +0530

    Add colleshpsing functions

--- app/src/main/java/com/smarthub/baseapplication/ui/utilites/fragment/AC1UtilitesFrag.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/utilites/fragment/AC1UtilitesFrag.kt
@@ -26,19 +26,41 @@ class AC1UtilitesFrag :Fragment(), ImageAttachmentAdapter.ItemClickListener {
         super.onViewCreated(view, savedInstanceState)
         var recyclerListener = view.findViewById<RecyclerView>(R.id.list_item)
         var collapsing_layout = view.findViewById<LinearLayout>(R.id.collapsing_layout)
+        var collapsing_InstallationAcceptance = view.findViewById<LinearLayout>(R.id.collapsing_InstallationAcceptance)
         var collapsing_layoutup = view.findViewById<LinearLayout>(R.id.collapsing_layoutup)
+        var collapsing_MaintenanceUp = view.findViewById<LinearLayout>(R.id.collapsing_MaintenanceUp)
+        var collapsing_InstallationAcceptanceUp = view.findViewById<LinearLayout>(R.id.collapsing_InstallationAcceptanceUp)
         var item_collapse_equipment = view.findViewById<LinearLayout>(R.id.item_collapse_equipment)
-        collapsing_layout.setOnClickListener(){
+        var item_collapse_installations = view.findViewById<LinearLayout>(R.id.item_collapse_installations)
+        var item_collapse_maintance = view.findViewById<LinearLayout>(R.id.item_collapse_maintance)
+         //Eqipment collapsing
+           collapsing_layout.setOnClickListener(){
             item_collapse_equipment.setVisibility(View.VISIBLE);
             collapsing_layoutup.setVisibility(View.VISIBLE);
             collapsing_layout.setVisibility(View.GONE);
 
         }
-
            collapsing_layoutup.setOnClickListener(){
             item_collapse_equipment.setVisibility(View.GONE);
             collapsing_layoutup.setVisibility(View.GONE);
             collapsing_layout.setVisibility(View.VISIBLE);
+        }
+        //installation collapsing
+            collapsing_InstallationAcceptance.setOnClickListener(){
+            item_collapse_installations.setVisibility(View.VISIBLE);
+            collapsing_InstallationAcceptanceUp.setVisibility(View.VISIBLE);
+            collapsing_InstallationAcceptance.setVisibility(View.GONE);
+
+
+
+        }
+            collapsing_InstallationAcceptanceUp.setOnClickListener(){
+            item_collapse_installations.setVisibility(View.GONE);
+            collapsing_InstallationAcceptanceUp.setVisibility(View.GONE);
+            collapsing_InstallationAcceptance.setVisibility(View.VISIBLE);
+
+
+
         }
 
         var adapter =  ImageAttachmentAdapter(this@AC1UtilitesFrag)
