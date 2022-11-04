18bb705da2efd0ffe31a2a4438902b38b852fe7c
tanya <Tanya.Raj@smartmile.com>
2022-11-04 22:27:57 +0900

    Commit Site Pictures button

---
+++ app/src/main/java/com/smarthub/baseapplication/ui/basic_info/SiteImages.kt
@@ -0,0 +1,14 @@
+package com.smarthub.baseapplication.ui.basic_info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smarthub.baseapplication.R
+import com.smarthub.baseapplication.databinding.BasicInfoDetailActivityBinding
+
+class SiteImages : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_gallery)
+
+    }
+}
