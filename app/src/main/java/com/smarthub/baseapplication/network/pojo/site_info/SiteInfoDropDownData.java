7fbd2f4004261cc3bff6a9fc8f60063ebe6bedad
somnath <somnathbca14@gmail.com>
2022-11-16 13:54:15 +0530

    register ui touchup phase one

--- app/src/main/java/com/smarthub/baseapplication/network/pojo/site_info/SiteInfoDropDownData.java
+++ app/src/main/java/com/smarthub/baseapplication/network/pojo/site_info/SiteInfoDropDownData.java
@@ -24,7 +24,7 @@ public class SiteInfoDropDownData {
 //    @Expose
 //    private AcuationsLeaseModel acquistionMode;
 
-    @SerializedName("AcquistionMode")
+  /*  @SerializedName("AcquistionMode")
     @Expose
     private AcuationsLeaseModel acuationsLeaseModel;
     @Expose
@@ -47,7 +47,7 @@ public class SiteInfoDropDownData {
     @SerializedName("comercial")
     @Expose
     private ComercialModel comercial;
-
+*/
     public BasicInfoModel getBasicInfoModel() {
         return basicInfoModel;
     }
@@ -81,67 +81,4 @@ public class SiteInfoDropDownData {
         this.safetyAndAccess = safetyAndAccess;
     }
 
-    public AcuationsLeaseModel getAcuationsLeaseModel() {
-        return acuationsLeaseModel;
-    }
-
-    public void setAcuationsLeaseModel(AcuationsLeaseModel acuationsLeaseModel) {
-        this.acuationsLeaseModel = acuationsLeaseModel;
-    }
-
-    public UtilitesNocModel getUtilitesNocModel() {
-        return utilitesNocModel;
-    }
-
-    public void setUtilitesNocModel(UtilitesNocModel utilitesNocModel) {
-        this.utilitesNocModel = utilitesNocModel;
-    }
-
-    public UtilitesNocModel getUtilities() {
-        return utilities;
-    }
-
-    public void setUtilities(UtilitesNocModel utilities) {
-        this.utilities = utilities;
-    }
-
-    public OpcoinfoModel getOpcoinfo() {
-        return opcoinfo;
-    }
-
-    public void setOpcoinfo(OpcoinfoModel opcoinfo) {
-        this.opcoinfo = opcoinfo;
-    }
-
-    public RfEquipmentModel getRfEquipment() {
-        return rfEquipment;
-    }
-
-    public void setRfEquipment(RfEquipmentModel rfEquipment) {
-        this.rfEquipment = rfEquipment;
-    }
-
-    public BackhaullinkModel getBackhaullink() {
-        return backhaullink;
-    }
-
-    public void setBackhaullink(BackhaullinkModel backhaullink) {
-        this.backhaullink = backhaullink;
-    }
-
-    public RfAntenaModel getRfAntena() {
-        return rfAntena;
-    }
-
-    public void setRfAntena(RfAntenaModel rfAntena) {
-        this.rfAntena = rfAntena;
-    }
-
-    public ComercialModel getComercial() {
-        return comercial;
-    }
-
-    public void setComercial(ComercialModel comercial) {
-        this.comercial = comercial;
-    }
-}
+  }
