300f4013cf6ab41077972aa61cd0d2c3d94f60ec
ankit <ankitkmaurya.iftmu@gmai.com>
2022-11-21 20:07:36 +0530

    login section update

--- app/src/main/java/com/smarthub/baseapplication/ui/fragments/forgot_password/ForgotPassStep1.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/forgot_password/ForgotPassStep1.kt
@@ -1,16 +1,23 @@
 package com.smarthub.baseapplication.ui.fragments.forgot_password
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.smarthub.baseapplication.R
-import com.smarthub.baseapplication.databinding.ForgotPassStep2FragmentBinding
-import com.smarthub.baseapplication.databinding.ForgotPasswordFragmentBinding
+import com.smarthub.baseapplication.databinding.ForgotPassStep1FragmentBinding
+import com.smarthub.baseapplication.model.otp.UserOTPGet
+import com.smarthub.baseapplication.utils.AppConstants
 import com.smarthub.baseapplication.utils.Utils
+import com.smarthub.baseapplication.viewmodels.LoginViewModel
 
 
 /**
@@ -20,66 +27,91 @@ import com.smarthub.baseapplication.utils.Utils
  */
 @Suppress("DEPRECATION")
 class ForgotPassStep1 : Fragment() {
-    var binding : ForgotPassStep2FragmentBinding?=null
+
+    private var loginViewModel : LoginViewModel?=null
+    private lateinit var progressDialog : ProgressDialog
+    lateinit var binding : ForgotPassStep1FragmentBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = inflater.inflate(R.layout.forgot_pass_step2_fragment, container, false)
-        binding = ForgotPassStep2FragmentBinding.bind(view)
-        return view
-
-    }
-
-    fun enableErrorMsg(){
-        binding?.errorMessage?.visibility = View.VISIBLE
+        binding = ForgotPassStep1FragmentBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loginViewModel = ViewModelProvider(requireActivity())[LoginViewModel::class.java]
+        progressDialog = ProgressDialog(requireContext())
+        progressDialog.setMessage("Please Wait...")
+        progressDialog.setCanceledOnTouchOutside(true)
 
-        view.findViewById<View>(R.id.back).setOnClickListener { view ->
+        binding?.back?.setOnClickListener {view->
             Utils.hideKeyboard(requireContext(),view)
             activity?.let{
-                it.onBackPressed()
+                it?.onBackPressed()
             }
         }
 
-        val regFragment2 = ForgotPassStep5()
-        view.findViewById<View>(R.id.next_layout).setOnClickListener {
+        binding?.signWithPhone?.setOnClickListener {
             Utils.hideKeyboard(requireContext(),it)
-            if(binding?.moNoEdit?.text!=null && !binding?.moNoEdit?.text?.toString().isNullOrEmpty() &&
-                binding?.moNoEdit?.text?.toString()?.length == 10) {
-                activity?.let {
-                    addFragment(regFragment2)
-                }
-            }else enableErrorMsg()
+            if (!progressDialog.isShowing)
+                progressDialog.show()
+            loginViewModel?.getPhoneOtp(UserOTPGet(binding?.moNoEdit?.text?.toString()))
         }
 
         binding?.moNoEdit?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable) {
-                if (binding?.moNoEdit?.text.toString().isNotEmpty() && binding?.moNoEdit?.text.toString().length>=10)
-                    Utils.hideKeyboard(requireContext(),binding?.moNoEdit!!)
+                if (binding?.moNoEdit?.text.toString().isNotEmpty() && binding?.moNoEdit?.text.toString().length>=10) {
+                    Utils.hideKeyboard(requireContext(), binding?.moNoEdit!!)
+                    binding?.signWithPhone?.visibility = View.VISIBLE
+                    binding?.signWithPhoneDisable?.visibility = View.GONE
+                }else{
+                    binding?.signWithPhoneDisable?.visibility = View.VISIBLE
+                    binding?.signWithPhone?.visibility = View.GONE
+                }
             }
         })
-    }
 
-    fun addFragment(fragment: Fragment?) {
-        val backStateName: String = requireActivity().supportFragmentManager.javaClass.name
-        val manager = requireActivity().supportFragmentManager
-        val fragmentPopped = manager.popBackStackImmediate(backStateName, 0)
-        if (!fragmentPopped) {
-            val transaction = manager.beginTransaction()
-            transaction.setCustomAnimations(
-                R.anim.enter,
-                R.anim.exit,
-                R.anim.pop_enter,
-                R.anim.pop_exit
-            )
-            transaction.replace(R.id.fragmentContainerView, fragment!!)
-            transaction.addToBackStack(backStateName)
-            transaction.commit()
+        loginViewModel?.getOtpResponse?.observe(requireActivity()) {
+            if (progressDialog.isShowing)
+                progressDialog.dismiss()
+
+            if (it?.data != null && it.data.sucesss == true){
+                Log.d("status","getOtpResponse ${it.data}")
+                activity?.let{
+                    findNavController().navigate(ForgotPassStep1Directions.actionForgotPassStep1ToForgotPassStep2())
+//                    val regFragment = ForgotPassStep2()
+//                    addFragment(regFragment)
+                }
+            }else{
+                Log.d("status","${AppConstants.GENERIC_ERROR}")
+                Toast.makeText(requireActivity(), AppConstants.GENERIC_ERROR, Toast.LENGTH_LONG).show()
+                enableErrorText()
+            }
         }
     }
+
+    private fun enableErrorText(){
+        binding?.userMailLayout?.error = "enter valid mo no"
+    }
+
+//    fun addFragment(fragment: Fragment?) {
+//        val backStateName: String = requireActivity().supportFragmentManager.javaClass.name
+//        val manager = requireActivity().supportFragmentManager
+//        val fragmentPopped = manager.popBackStackImmediate(backStateName, 0)
+//        if (!fragmentPopped) {
+//            val transaction = manager.beginTransaction()
+//            transaction.setCustomAnimations(
+//                R.anim.enter,
+//                R.anim.exit,
+//                R.anim.pop_enter,
+//                R.anim.pop_exit
+//            )
+//            transaction.replace(R.id.fragmentContainerView, fragment!!)
+//            transaction.addToBackStack(backStateName)
+//            transaction.commit()
+//        }
+//    }
 
 }
 
---
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/login/LoginFragment.kt
@@ -0,0 +1,110 @@
+package com.smarthub.baseapplication.ui.fragments.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.smarthub.baseapplication.R
+import com.smarthub.baseapplication.activities.BaseActivity
+import com.smarthub.baseapplication.activities.DashboardActivity
+import com.smarthub.baseapplication.databinding.LoginFragmentBinding
+import com.smarthub.baseapplication.helpers.AppPreferences
+import com.smarthub.baseapplication.helpers.Resource
+import com.smarthub.baseapplication.model.login.UserLoginPost
+import com.smarthub.baseapplication.network.User
+import com.smarthub.baseapplication.utils.AppConstants
+import com.smarthub.baseapplication.utils.Utils
+import com.smarthub.baseapplication.viewmodels.LoginViewModel
+
+
+class LoginFragment : Fragment() {
+
+    private var loginViewModel : LoginViewModel?=null
+    private var user : User?=null
+    var binding : LoginFragmentBinding?=null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        var view = inflater.inflate(R.layout.login_fragment, container, false)
+        binding = LoginFragmentBinding.bind(view)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        binding?.login?.setOnClickListener {
+            Utils.hideKeyboard(requireContext(),it)
+            loginValidation()
+        }
+        binding?.textRegister?.setOnClickListener {
+            Utils.hideKeyboard(requireContext(),it)
+//            val regFragment1 = RegistrationFirstStep()
+//            addFragment(regFragment1)
+        }
+
+        binding?.forgotPassword?.setOnClickListener {
+            Utils.hideKeyboard(requireContext(),it)
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToForgotPassStep1())
+//            val regFragment1 = ForgotPassStep1()
+//            addFragment(regFragment1)
+        }
+
+        binding?.signWithPhone?.setOnClickListener {
+            Utils.hideKeyboard(requireContext(),it)
+//            val regFragment1 = OtpVerificationStep2()
+//            addFragment(regFragment1)
+        }
+    }
+
+    private fun loginValidation(){
+        loginViewModel?.loginResponse?.observe(requireActivity()) {
+            (requireActivity() as BaseActivity).hideLoader()
+            if (it != null && it.data?.access?.isNotEmpty() == true) {
+                if (it.status == Resource.Status.SUCCESS && it.data!=null) {
+                    AppPreferences.getInstance().saveString("accessToken", "${it.data?.access}")
+                    AppPreferences.getInstance().saveString("refreshToken", "${it.data?.refresh}")
+
+
+                    Toast.makeText(requireContext(),"LoginSuccessful",Toast.LENGTH_LONG).show()
+                    val intent = Intent (requireContext(), DashboardActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    requireActivity().finish()
+                    return@observe
+                }else{
+
+                    Toast.makeText(requireContext(),"error:"+it.message,Toast.LENGTH_LONG).show()
+                    enableErrorMsg()
+//                    loginViewModel?.loginResponse?.removeObservers(this)
+//                    val regFragment1 = LoginSecondStep()
+//                    addWithoutStackFragment(regFragment1)
+                }
+            }
+            else{
+                Toast.makeText(requireContext(),AppConstants.GENERIC_ERROR,Toast.LENGTH_LONG).show()
+                enableErrorMsg()
+//                loginViewModel?.loginResponse?.removeObservers(this)
+//                val regFragment1 = LoginSecondStep()
+//                addWithoutStackFragment(regFragment1)
+
+            }
+
+        }
+        user?.username = binding?.userMail?.text.toString()
+        (requireActivity() as BaseActivity).showLoader()
+        loginViewModel?.getLoginToken(UserLoginPost(binding?.userMail?.text.toString(),binding?.password?.text.toString()))
+    }
+
+    private fun enableErrorMsg(){
+        binding?.userMailLayout?.error = "Invalid username"
+        binding?.passwordLayout?.error = "Incorrect password"
+    }
+}
+
+
