a319084f3a2be49969b3afba6d62e969fdb232ca
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-11-26 16:07:22 +0530

    basic bottom sheet

--- app/src/main/java/com/smarthub/baseapplication/ui/fragments/customer_tab/SiteInfoNewFragment.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/customer_tab/SiteInfoNewFragment.kt
@@ -9,12 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.SiteInfoNewFragmentBinding
-import com.smarthub.baseapplication.ui.dialog.opco.DetailsBottomSheet
+import com.smarthub.baseapplication.ui.dialog.opco.BasicInfoBottomSheet
 import com.smarthub.baseapplication.ui.fragments.siteInfo.SiteInfoListAdapter
 import com.smarthub.baseapplication.viewmodels.BasicInfoDetailViewModel
 
 class SiteInfoNewFragment :Fragment(), SiteInfoListAdapter.SiteInfoLisListener {
-    var bottomSheetDialogFragment : DetailsBottomSheet?=null
     var binding : SiteInfoNewFragmentBinding?=null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -37,23 +36,23 @@ class SiteInfoNewFragment :Fragment(), SiteInfoListAdapter.SiteInfoLisListener {
         Toast.makeText(requireContext(),"Item Clicked",Toast.LENGTH_SHORT).show()
     }
     override fun detailsItemClicked() {
-        bottomSheetDialogFragment = DetailsBottomSheet(R.layout.basic_info_details_bottom_sheet)
+        var bottomSheetDialogFragment = BasicInfoBottomSheet(R.layout.basic_info_details_bottom_sheet)
         bottomSheetDialogFragment?.show(childFragmentManager,"category")
 
     }
     override fun operationInfoDetailsItemClicked() {
-        bottomSheetDialogFragment = DetailsBottomSheet(R.layout.operations_info_details_bottom_sheet)
+        var bottomSheetDialogFragment = BasicInfoBottomSheet(R.layout.operations_info_details_bottom_sheet)
         bottomSheetDialogFragment?.show(childFragmentManager,"category")
 
     }
     override fun geoConditionsDetailsItemClicked() {
-        bottomSheetDialogFragment = DetailsBottomSheet(R.layout.geo_conditions_details_bottom_sheet)
+        var  bottomSheetDialogFragment = BasicInfoBottomSheet(R.layout.geo_conditions_details_bottom_sheet)
         bottomSheetDialogFragment?.show(childFragmentManager,"category")
 
     }
 
     override fun siteAccessDetailsItemClicked() {
-        bottomSheetDialogFragment = DetailsBottomSheet(R.layout.site_access_details_bottom_sheet)
+        var  bottomSheetDialogFragment = BasicInfoBottomSheet(R.layout.site_access_details_bottom_sheet)
         bottomSheetDialogFragment?.show(childFragmentManager,"category")
     }
 
