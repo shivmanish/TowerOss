dd43cac6ec7b9d6f57e439a1f7a5060fe1754246
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-11-26 12:31:31 +0530

    nominals changes editview

--- app/src/main/java/com/smarthub/baseapplication/ui/site_lease_acquisition/fragment/TeamVendor.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_lease_acquisition/fragment/TeamVendor.kt
@@ -6,13 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.TeamVendorFragmentBinding
-import com.smarthub.baseapplication.ui.adapter.common.ImageAttachmentAdapter
+import com.smarthub.baseapplication.ui.dialog.opco.DetailsBottomSheet
 import com.smarthub.baseapplication.ui.site_lease_acquisition.adapter.Team_VendorLeaseListAdapter
 
-class TeamVendor :Fragment(), ImageAttachmentAdapter.ItemClickListener {
-
+class TeamVendor :Fragment(), Team_VendorLeaseListAdapter.TeamVendorListItemListner {
     var binding : TeamVendorFragmentBinding?=null
+    var bottomSheetDialogFragment : DetailsBottomSheet?=null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = TeamVendorFragmentBinding.inflate(inflater, container, false)
         return binding?.root
@@ -20,10 +21,16 @@ class TeamVendor :Fragment(), ImageAttachmentAdapter.ItemClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.teamVendorList?.adapter = Team_VendorLeaseListAdapter(this@TeamVendor)
-    }
-
-
-    override fun itemClicked() {
+      }
+      override fun attachmentItemClicked() {
         Toast.makeText(requireContext(),"Item Clicked",Toast.LENGTH_SHORT).show()
+     }
+    override fun detailsItemClicked() {
+        if (bottomSheetDialogFragment==null) {
+            bottomSheetDialogFragment = DetailsBottomSheet(R.layout.teamvender_details_botom_sheet)
+            bottomSheetDialogFragment?.show(childFragmentManager,"category")
+        }else bottomSheetDialogFragment?.show(childFragmentManager,"category")
+
     }
+
 }
