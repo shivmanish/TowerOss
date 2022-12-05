3485bf4cb8a393dd16704336cbb027ff39eee88b
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-12-05 23:14:41 +0530

    DG Utills Screen changes

--- app/src/main/java/com/smarthub/baseapplication/ui/utilites/DGDetailsActivity.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/utilites/DGDetailsActivity.kt
@@ -3,17 +3,12 @@ package com.smarthub.baseapplication.ui.utilites
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.smarthub.baseapplication.databinding.ActivityAcDetailsBinding
-import com.smarthub.baseapplication.databinding.ActivityBatteryBankDetailsBinding
-import com.smarthub.baseapplication.databinding.ActivityDetailActivityBinding
 import com.smarthub.baseapplication.databinding.ActivityDgDetailsBinding
-import com.smarthub.baseapplication.databinding.ActivityFireExtinguisherDetailsBinding
 import com.smarthub.baseapplication.ui.utilites.adapter.BatteryViewpagerAdapter
-import com.smarthub.baseapplication.ui.utilites.fragment.AcFragment
-import com.smarthub.baseapplication.ui.utilites.fragment.BatteryFragment
-import com.smarthub.baseapplication.ui.utilites.fragment.FireExtinguisherFragment
+import com.smarthub.baseapplication.ui.utilites.adapter.DGViewpagerAdapter
+import com.smarthub.baseapplication.ui.utilites.fragment.DGFragment
 
-class DgDetailsActivity : AppCompatActivity() {
+class DGDetailsActivity : AppCompatActivity() {
     lateinit var binding:ActivityDgDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,12 +19,12 @@ class DgDetailsActivity : AppCompatActivity() {
 
     fun initview(){
         val fragmentlist = ArrayList<Fragment>()
-        fragmentlist.add(FireExtinguisherFragment())
-        fragmentlist.add(FireExtinguisherFragment())
+        fragmentlist.add(DGFragment())
+        fragmentlist.add(DGFragment())
         val titels = ArrayList<String>()
         titels.add("DG#1")
         titels.add("DG#2")
-        binding.viewpager.adapter = BatteryViewpagerAdapter(supportFragmentManager,fragmentlist,titels)
+        binding.viewpager.adapter = DGViewpagerAdapter(supportFragmentManager,fragmentlist,titels)
         binding.tabs.setupWithViewPager(binding.viewpager)
     }
 
