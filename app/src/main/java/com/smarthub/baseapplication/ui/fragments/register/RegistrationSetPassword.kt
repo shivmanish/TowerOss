1e330b37c47f70a5266eb041593f03c22e1a0d2d
manis <manish.sharma@smartmile.com>
2022-12-07 19:37:09 +0530

    registration set password screen

--- app/src/main/java/com/smarthub/baseapplication/ui/fragments/register/RegistrationSetPassword.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/register/RegistrationSetPassword.kt
@@ -13,6 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.smarthub.baseapplication.databinding.RegistrationSetPassBinding
+import com.smarthub.baseapplication.databinding.RegistrationSetPasswordBinding
 import com.smarthub.baseapplication.utils.AppConstants
 import com.smarthub.baseapplication.utils.Utils
 import com.smarthub.baseapplication.viewmodels.LoginViewModel
@@ -21,10 +22,10 @@ class RegistrationSetPassword : Fragment() {
 
     private var loginViewModel : LoginViewModel?=null
     private lateinit var progressDialog : ProgressDialog
-    lateinit var binding : RegistrationSetPassBinding
+    lateinit var binding : RegistrationSetPasswordBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = RegistrationSetPassBinding.inflate(inflater)
+        binding = RegistrationSetPasswordBinding.inflate(inflater)
         return binding.root
     }
     var phone = "1234567890"
@@ -41,15 +42,16 @@ class RegistrationSetPassword : Fragment() {
         progressDialog.setMessage("Please Wait...")
         progressDialog.setCanceledOnTouchOutside(true)
 
-        binding.confirmPass.addTextChangedListener(object : TextWatcher {
+        binding.confirmPassword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable) {
-                if (binding.moNoEdit.text.toString().isNotEmpty() && binding.moNoEdit.text.toString() == binding.confirmPass.text.toString()) {
+                if (binding.moNoEdit.text.toString().isNotEmpty() && binding.moNoEdit.text.toString() == binding.confirmPassword.text.toString()) {
                     Utils.hideKeyboard(requireContext(), binding.moNoEdit)
-                    binding.signWithPhone.alpha = 1.0f
+                    binding.submitPass.isClickable=true
+                    binding.submitPass.alpha = 1.0f
                 }else{
-                    binding.signWithPhone.alpha = 0.3f
+                    binding.submitPass.alpha = 0.3f
                 }
             }
         })
@@ -73,7 +75,7 @@ class RegistrationSetPassword : Fragment() {
                 enableErrorText()
             }
         }
-        binding.signWithPhone.setOnClickListener {
+        binding.submitPass.setOnClickListener {
             Utils.hideKeyboard(requireContext(),it)
             if (!progressDialog.isShowing)
                 progressDialog.show()
@@ -82,11 +84,11 @@ class RegistrationSetPassword : Fragment() {
     }
 
     private fun enableErrorText(){
-        binding.userMailLayout.error = "enter valid mo no"
+        binding.passwordLayout.error = "enter valid mo no"
     }
 
     private fun enableErrorText(error:String?){
-        binding.userMailLayout.error = "$error"
+        binding.confirmPassLayout.error = "$error"
     }
 }
 
