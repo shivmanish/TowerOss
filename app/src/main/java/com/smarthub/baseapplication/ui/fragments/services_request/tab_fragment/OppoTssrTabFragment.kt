ec0eeef9ae2edef098260293cd48bc3c251806b3
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-12-10 14:31:42 +0530

    table view changes

---
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/services_request/tab_fragment/OppoTssrTabFragment.kt
@@ -0,0 +1,57 @@
+package com.smarthub.baseapplication.ui.fragments.services_request.tab_fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.smarthub.baseapplication.R
+import com.smarthub.baseapplication.databinding.OpcoInfoFregmentBinding
+import com.smarthub.baseapplication.ui.dialog.services_request.*
+import com.smarthub.baseapplication.ui.fragments.opcoInfo.OpcoInfoListAdapter
+import com.smarthub.baseapplication.ui.fragments.opcoInfo.OpcoSiteInfoEditDialouge
+import com.smarthub.baseapplication.ui.fragments.opcoInfo.OperationsItemsEditDialouge
+import com.smarthub.baseapplication.ui.fragments.services_request.ServicesRequestAdapter
+import com.smarthub.baseapplication.ui.utilites.editdialouge.ServicesDetailsBottomSheetDialog
+import com.smarthub.baseapplication.viewmodels.SiteInfoViewModel
+
+
+
+
+class OppoTssrTabFragment : Fragment(), ServicesRequestAdapter.ServicesRequestLisListener {
+    var siteViewModel : SiteInfoViewModel?=null
+    var binding : OpcoInfoFregmentBinding?=null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = OpcoInfoFregmentBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.listItem?.adapter = ServicesRequestAdapter(this@OppoTssrTabFragment)
+    }
+    override fun attachmentItemClicked() {
+
+    }
+    override fun detailsItemClicked() {
+        val bottomSheetDialogFragment = SRDetailsBottomSheet(R.layout.rf_feasibility_bottom_sheet_dialog)
+        bottomSheetDialogFragment.show(childFragmentManager,"category")
+    }
+    override fun operationInfoDetailsItemClicked() {
+        val bottomSheetDialogFragment = EquipmentDetailsBottomSheetDialog(R.layout.equipment_bottom_sheet)
+        bottomSheetDialogFragment.show(childFragmentManager,"category")
+    }
+    override fun geoConditionsDetailsItemClicked() {
+        val bottomSheetDialogFragment = RadioAntennasBottomSheetDialog(R.layout.radio_antina_bottom_sheet_dialog)
+        bottomSheetDialogFragment.show(childFragmentManager,"category")
+    }
+    override fun siteAccessDetailsItemClicked() {
+        val bottomSheetDialogFragment = BachhualLinkBottomSheet(R.layout.backhaul_link_list_item)
+        bottomSheetDialogFragment.show(childFragmentManager,"category")
+    }
+    override fun requestinfoClicked() {
+        val bottomSheetDialogFragment = RequestInfoBottomSheet(R.layout.request_info_bottom_sheet_dialog)
+        bottomSheetDialogFragment.show(childFragmentManager,"category")
+    }
+
+
+}
