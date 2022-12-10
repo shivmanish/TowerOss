b656b9c6b02b7b0ec48d20819f1f0671df8da3b4
tanya <Tanya.Raj@smartmile.com>
2022-12-10 21:59:07 +0900

    SMPS Utilities fragment tabs completed

--- app/src/main/java/com/smarthub/baseapplication/ui/utilites/fragment/SMPS1UitilitiesFrag.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/utilites/fragment/SMPS1UitilitiesFrag.kt
@@ -9,15 +9,20 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.Ac1TabUtilitiesFragmentBinding
 import com.smarthub.baseapplication.databinding.BatteryFragmentBinding
 import com.smarthub.baseapplication.databinding.Smps1TabUtilitiesFragmentBinding
 import com.smarthub.baseapplication.ui.adapter.common.ImageAttachmentAdapter
+import com.smarthub.baseapplication.ui.dialog.utils.ConnectedLoadsBottomSheetDialog
 import com.smarthub.baseapplication.ui.dialog.utils.RectifierModuleBottomSheetDialog
+import com.smarthub.baseapplication.ui.utilites.adapter.SMPSViewRecyclerAdapter
 import com.smarthub.baseapplication.ui.utilites.editdialouge.BatteryEquipmentDialouge
+import com.smarthub.baseapplication.ui.utilites.editdialouge.ConsumableMaterialsBottomSheetDialog
 import com.smarthub.baseapplication.ui.utilites.editdialouge.InstalationAcceptanceDialouge
+import com.smarthub.baseapplication.ui.utilites.editdialouge.PoDetailsBottomSheetDialog
 import com.smarthub.baseapplication.utils.Utils
 
 class SMPS1UitilitiesFrag: Fragment() {
@@ -47,6 +52,22 @@ class SMPS1UitilitiesFrag: Fragment() {
             val dalouge = RectifierModuleBottomSheetDialog()
             dalouge.show(childFragmentManager,"")
         }
+        binding.rectifierModuleRecycler.rv.adapter=SMPSViewRecyclerAdapter()
+        binding.connectedLoadsEdit.setOnClickListener{
+            val dalouge = ConnectedLoadsBottomSheetDialog()
+            dalouge.show(childFragmentManager,"")
+        }
+        binding.connectedLoadsRecycler.rv.adapter=SMPSViewRecyclerAdapter()
+        binding.consumableMaterialsEdit.setOnClickListener{
+            val dalouge = ConsumableMaterialsBottomSheetDialog()
+            dalouge.show(childFragmentManager,"")
+        }
+        binding.consumableMaterialsRecycler.rv.adapter=SMPSViewRecyclerAdapter()
+        binding.poDetailsEdit.setOnClickListener{
+            val dalouge = PoDetailsBottomSheetDialog()
+            dalouge.show(childFragmentManager,"")
+        }
+        binding.poDetailsRecycler.rv.adapter=SMPSViewRecyclerAdapter()
         binding.equipmentRoot.setOnClickListener {
             if(binding.itemCollapseEquipment.visibility == View.VISIBLE){
                 Utils.collapse(binding.itemCollapseEquipment)
@@ -81,11 +102,66 @@ class SMPS1UitilitiesFrag: Fragment() {
                 binding.rectifierModuleRoot.isSelected = false
                 binding.rectifierModuleArrow.rotation = 0f
                 binding.rectifierModuleEdit.visibility = View.GONE
+                binding.rectifierModuleDelete.visibility = View.GONE
+                binding.rectifierModuleAdd.visibility = View.GONE
             }else{
                 Utils.expand(binding.rectifierModuleCollapse)
                 binding.rectifierModuleRoot.isSelected = true
                 binding.rectifierModuleArrow.rotation = 180f
                 binding.rectifierModuleEdit.visibility = View.VISIBLE
+                binding.rectifierModuleDelete.visibility = View.VISIBLE
+                binding.rectifierModuleAdd.visibility = View.VISIBLE
+            }
+        }
+        binding.connectedLoadsRoot.setOnClickListener {
+            if(binding.connectedLoadsCollapse.visibility == View.VISIBLE){
+                Utils.collapse(binding.connectedLoadsCollapse)
+                binding.connectedLoadsRoot.isSelected = false
+                binding.connectedLoadsArrow.rotation = 0f
+                binding.connectedLoadsEdit.visibility = View.GONE
+                binding.connectedLoadsDelete.visibility = View.GONE
+                binding.connectedLoadsAdd.visibility = View.GONE
+            }else{
+                Utils.expand(binding.connectedLoadsCollapse)
+                binding.connectedLoadsRoot.isSelected = true
+                binding.connectedLoadsArrow.rotation = 180f
+                binding.connectedLoadsEdit.visibility = View.VISIBLE
+                binding.connectedLoadsDelete.visibility = View.VISIBLE
+                binding.connectedLoadsAdd.visibility = View.VISIBLE
+            }
+        }
+        binding.consumableMaterialsRoot.setOnClickListener {
+            if(binding.consumableMaterialsCollapse.visibility == View.VISIBLE){
+                Utils.collapse(binding.consumableMaterialsCollapse)
+                binding.consumableMaterialsRoot.isSelected = false
+                binding.consumableMaterialsArrow.rotation = 0f
+                binding.consumableMaterialsEdit.visibility = View.GONE
+                binding.consumableMaterialsDelete.visibility = View.GONE
+                binding.consumableMaterialsAdd.visibility = View.GONE
+            }else{
+                Utils.expand(binding.consumableMaterialsCollapse)
+                binding.consumableMaterialsRoot.isSelected = true
+                binding.consumableMaterialsArrow.rotation = 180f
+                binding.consumableMaterialsEdit.visibility = View.VISIBLE
+                binding.consumableMaterialsDelete.visibility = View.VISIBLE
+                binding.consumableMaterialsAdd.visibility = View.VISIBLE
+            }
+        }
+        binding.poDetailsRoot.setOnClickListener {
+            if(binding.poDetailsCollapse.visibility == View.VISIBLE){
+                Utils.collapse(binding.poDetailsCollapse)
+                binding.poDetailsRoot.isSelected = false
+                binding.poDetailsArrow.rotation = 0f
+                binding.poDetailsEdit.visibility = View.GONE
+                binding.poDetailsDelete.visibility = View.GONE
+                binding.poDetailsAdd.visibility = View.GONE
+            }else{
+                Utils.expand(binding.poDetailsCollapse)
+                binding.poDetailsRoot.isSelected = true
+                binding.poDetailsArrow.rotation = 180f
+                binding.poDetailsEdit.visibility = View.VISIBLE
+                binding.poDetailsDelete.visibility = View.VISIBLE
+                binding.poDetailsAdd.visibility = View.VISIBLE
             }
         }
 
