e48e948b780a999a22099845c289c5010e2f60a0
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-12-14 23:15:39 +0530

    Feasibility planning

---
+++ app/src/main/java/com/smarthub/baseapplication/ui/dialog/services_request/CommersalSiteProposalBottomSheet.kt
@@ -0,0 +1,28 @@
+package com.smarthub.baseapplication.ui.dialog.services_request
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.smarthub.baseapplication.R
+import com.smarthub.baseapplication.databinding.BackhaulLinkListItemDialougeBinding
+import com.smarthub.baseapplication.databinding.GenerateSiteProposalDialogBinding
+import com.smarthub.baseapplication.databinding.PowerMsbBottomSheetDialogBinding
+import com.smarthub.baseapplication.databinding.TssrExecutiveBottomSheetDialogBinding
+
+class CommersalSiteProposalBottomSheet(contentLayoutId: Int) : BottomSheetDialogFragment(contentLayoutId) {
+    lateinit var binding : GenerateSiteProposalDialogBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = GenerateSiteProposalDialogBinding.bind(view)
+        binding.icMenuClose.setOnClickListener {
+            dismiss()
+        }
+    }
+    override fun getTheme() = R.style.NewDialogTask
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = GenerateSiteProposalDialogBinding.inflate(inflater)
+        return binding.root
+    }
+}
