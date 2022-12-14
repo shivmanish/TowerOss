0ed9fb6c19f8f27e249edf4e3906eb755d0b5b28
tanya <Tanya.Raj@smartmile.com>
2022-12-15 00:45:48 +0900

    Logs Activity

---
+++ app/src/main/java/com/smarthub/baseapplication/ui/logs/LogsActivity.kt
@@ -0,0 +1,12 @@
+package com.smarthub.baseapplication.ui.logs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smarthub.baseapplication.R
+
+class LogsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logs)
+    }
+}
