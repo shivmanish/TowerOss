1078d9839d53433329ecd624cea2ddd7229ae60b
manis <manish.sharma@smartmile.com>
2022-12-16 19:46:57 +0530

    equipment room tables implementations

--- app/src/main/java/com/smarthub/baseapplication/ui/fragments/towerCivilInfra/TowerEquipmentInfoFragment.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/towerCivilInfra/TowerEquipmentInfoFragment.kt
@@ -10,6 +10,7 @@ import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.TowerPoleInfoFragmentBinding
 import com.smarthub.baseapplication.ui.fragments.opcoInfo.OpcoSiteInfoEditDialouge
 import com.smarthub.baseapplication.ui.fragments.opcoInfo.OperationsItemsEditDialouge
+import com.smarthub.baseapplication.ui.fragments.towerCivilInfra.bottomSheet.*
 
 class TowerEquipmentInfoFragment: Fragment(), TowerEquipmentInfoAdapter.TowerPoleListListener {
     var binding : TowerPoleInfoFragmentBinding?=null
@@ -21,7 +22,7 @@ class TowerEquipmentInfoFragment: Fragment(), TowerEquipmentInfoAdapter.TowerPol
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding?.listItem?.adapter = TowerEquipmentInfoAdapter(this@TowerEquipmentInfoFragment)
+        binding?.listItem?.adapter = TowerEquipmentInfoAdapter(requireContext(),this@TowerEquipmentInfoFragment)
     }
 
     override fun attachmentItemClicked() {
@@ -39,6 +40,28 @@ class TowerEquipmentInfoFragment: Fragment(), TowerEquipmentInfoAdapter.TowerPol
         val bottomSheetDialogFragment = OpcoSiteInfoEditDialouge(R.layout.opco_info_site_dialouge_layout)
         bottomSheetDialogFragment.show(childFragmentManager,"category")
 
+    }
+    override fun editPoClicked(position: Int) {
+        var bm = EquipmentPoEditDialougeAdapter(R.layout.tower_equipment_po_edit_dialouge)
+        bm.show(childFragmentManager, "category")
+        Toast.makeText(requireContext() , "Item 2 clicked" , Toast.LENGTH_SHORT).show()
+    }
+
+    override fun viewPoClicked(position: Int) {
+        var bm = EquipmentPoViewDialougeAdapter(R.layout.equipment_room_view_dialouge)
+        bm.show(childFragmentManager, "category")
+    }
+
+    override fun editConsumableClicked(position: Int) {
+        var bm = EarthingConsumableEditDialougeAdapter(R.layout.earthing_consumable_table_edit_dialouge)
+        bm.show(childFragmentManager, "category")
+        Toast.makeText(requireContext() , "Item 2 clicked" , Toast.LENGTH_SHORT).show()
+    }
+
+    override fun viewConsumableClicked(position: Int) {
+        var bm = EarthingConsumableTableViewDialougeAdapter(R.layout.earthing_consumable_table_view_dialouge)
+        bm.show(childFragmentManager, "category")
+        Toast.makeText(requireContext() , "Item 2 clicked" , Toast.LENGTH_SHORT).show()
     }
 
 
