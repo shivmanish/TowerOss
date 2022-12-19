c7e8ff3651ab893fdf89fd5e491410e0a1c1c836
manis <manish.sharma@smartmile.com>
2022-12-19 19:49:50 +0530

    notification screen imple,entation

---
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/notifications/NotificationListTitleAdapter.kt
@@ -0,0 +1,37 @@
+package com.smarthub.baseapplication.ui.fragments.notifications
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.smarthub.baseapplication.R
+import com.smarthub.baseapplication.databinding.NotificationListTitleBinding
+import com.smarthub.baseapplication.model.notification.NotificationData
+import com.smarthub.baseapplication.model.notification.NotificationListItem
+import com.smarthub.baseapplication.model.notification.NotificationListModel
+
+class NotificationListTitleAdapter(val context: Context) : Adapter<NotificationListTitleAdapter.Viewholder>() {
+
+    var list : ArrayList<NotificationListModel> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Viewholder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.notification_list_title, parent, false)
+        return Viewholder(view)
+    }
+
+    override fun onBindViewHolder(holder: Viewholder, position: Int) {
+
+        holder.binding.textList.text = list[position].title+"$position"
+        holder.binding.list.adapter = NotificationListSubtitleAdapter(context)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class Viewholder(item:View) : RecyclerView.ViewHolder(item) {
+        var binding = NotificationListTitleBinding.bind(item)
+    }
+}
