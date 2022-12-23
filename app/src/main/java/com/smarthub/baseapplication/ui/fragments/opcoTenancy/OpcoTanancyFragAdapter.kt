794b6e778d5065a7925cadf1c35879f7e21c1956
manis <manish.sharma@smartmile.com>
2022-12-23 11:05:46 +0530

    api implementation update on siteInfo Data model

--- app/src/main/java/com/smarthub/baseapplication/ui/fragments/opcoTenancy/OpcoTanancyFragAdapter.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/opcoTenancy/OpcoTanancyFragAdapter.kt
@@ -1,5 +1,6 @@
 package com.smarthub.baseapplication.ui.fragments.opcoTenancy
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,13 +10,13 @@ import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.CustomerListItemBinding
 import com.smarthub.baseapplication.model.siteInfo.opcoInfo.OpcoDataItem
 import com.smarthub.baseapplication.utils.AppController
-import com.smarthub.baseapplication.viewmodels.HomeViewModel
 
 class OpcoTanancyFragAdapter(var listener: CustomerDataAdapterListener) :
     Adapter<CustomerDataViewHolder>() {
     var data1 = AppController.getInstance().siteInfoModel.item
     var data : List<OpcoDataItem> = ArrayList()
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setOpData(data: List<OpcoDataItem>) {
         this.data = data
         notifyDataSetChanged()
@@ -23,10 +24,11 @@ class OpcoTanancyFragAdapter(var listener: CustomerDataAdapterListener) :
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomerDataViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.customer_list_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.customer_list_item, parent, false)
         return CustomerDataViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: CustomerDataViewHolder, position: Int) {
         holder.binding.SiteId.text= "${data1?.get(0)?.Basicinfo?.get(0)?.siteID}"
         holder.binding.textRfiDate.text= "RFI Date: ${data[position].Opcoinfo[0].rfiAcceptanceDate}"
