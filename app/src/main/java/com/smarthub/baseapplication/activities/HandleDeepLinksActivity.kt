b34fa0eb78ade6e0c6c5e1df34ccfe74ac4ba044
ankit <ankitkmaurya.iftmu@gmai.com>
2022-12-23 11:36:05 +0530

    deep link integration

---
+++ app/src/main/java/com/smarthub/baseapplication/activities/HandleDeepLinksActivity.kt
@@ -0,0 +1,12 @@
+package com.smarthub.baseapplication.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smarthub.baseapplication.R
+
+class HandleDeepLinksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_handle_deep_links)
+    }
+}
