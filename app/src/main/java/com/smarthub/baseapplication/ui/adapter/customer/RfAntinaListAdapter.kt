9643c90924a5b1c2142476cafbe962cd3307547f
manis <manish.sharma@smartmile.com>
2022-12-24 14:10:24 +0530

    api implementation on Rf Anteena tab in opcoTenency view mode update

--- app/src/main/java/com/smarthub/baseapplication/ui/adapter/customer/RfAntinaListAdapter.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/adapter/customer/RfAntinaListAdapter.kt
@@ -12,6 +12,7 @@ import com.smarthub.baseapplication.ui.fragments.customer_tab.CustomerInvoiceAda
 class RfAntinaListAdapter(var listener:  RfAnteenaItemClickListener) : RecyclerView.Adapter<RfAntinaListAdapter.ViewHold>() {
 
     var list : ArrayList<String> = ArrayList()
+    var currentOpened = -1
 
 
 
@@ -20,58 +21,72 @@ class RfAntinaListAdapter(var listener:  RfAnteenaItemClickListener) : RecyclerV
 
     }
 
-    class ViewHold(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHold(itemView: View,listener:RfAnteenaItemClickListener) : RecyclerView.ViewHolder(itemView) {
         var binding : RfAntinaListItemBinding = RfAntinaListItemBinding.bind(itemView)
+        var adapter =  ImageAttachmentAdapter(object : ImageAttachmentAdapter.ItemClickListener{
+            override fun itemClicked() {
+                listener.attachmentItemClicked()
+            }
+        })
         init {
-            binding.itemTitleDropdown.tag = false
-            if ((binding.itemTitleDropdown.tag as Boolean)) {
-                binding.listItemDropdown?.setImageResource(R.drawable.ic_arrow_up)
+            binding.collapsingLayout.tag = false
+            if ((binding.collapsingLayout.tag as Boolean)) {
+                binding.imgDropdown.setImageResource(R.drawable.ic_arrow_up)
                 binding.titleLayout.setBackgroundResource(R.drawable.bg_expansion_bar)
             } else {
-                binding.listItemDropdown?.setImageResource(R.drawable.ic_arrow_down_black)
+                binding.imgDropdown.setImageResource(R.drawable.ic_arrow_down_black)
                 binding.titleLayout.setBackgroundResource(R.color.collapse_card_bg)
             }
 
+            var recyclerListener = itemView.findViewById<RecyclerView>(R.id.list_item)
+            recyclerListener.adapter = adapter
 
-//
-//            itemView.findViewById<View>(R.id.attach_card).setOnClickListener {
-//                adapter.addItem()
-//            }
+            itemView.findViewById<View>(R.id.attach_card).setOnClickListener {
+                adapter.addItem()
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHold {
         var view = LayoutInflater.from(parent.context).inflate(R.layout.rf_antina_list_item,parent,false)
-        return ViewHold(view)
+        return ViewHold(view,listener)
     }
 
     override fun onBindViewHolder(holder: ViewHold, position: Int) {
-        holder.binding.itemTitleDropdown.setOnClickListener {
-            holder.binding.itemTitleDropdown.tag = !(holder.binding.itemTitleDropdown.tag as Boolean)
-            if ((holder.binding.itemTitleDropdown.tag as Boolean)) {
-                holder.binding.listItemDropdown.setImageResource(R.drawable.ic_arrow_up)
-                holder.binding.titleLayout.setBackgroundResource(R.drawable.bg_expansion_bar)
-            } else {
-                holder.binding.listItemDropdown.setImageResource(R.drawable.ic_arrow_down_black)
-                holder.binding.titleLayout.setBackgroundResource(R.color.collapse_card_bg)
-            }
-
-            holder.binding.editListItem.setOnClickListener {
-                listener.editModeCliked()
-            }
-
-            holder.binding.itemLine.visibility =
-                if (holder.binding.itemTitleDropdown.tag as Boolean) View.GONE else View.VISIBLE
-            holder.binding.itemCollapse.visibility =
-                if (holder.binding.itemTitleDropdown.tag as Boolean) View.VISIBLE else View.GONE
-            holder.binding.editListItem.visibility =
-                if (holder.binding.itemTitleDropdown.tag as Boolean) View.VISIBLE else View.INVISIBLE
+        holder.binding.imgEdit.setOnClickListener {
+            listener.editModeCliked()
+        }
+        holder.binding.collapsingLayout.setOnClickListener {
+            updateList(position)
+        }
+        if(currentOpened==position){
+            holder.binding.imgDropdown.setImageResource(R.drawable.ic_arrow_up)
+            holder.binding.titleLayout.setBackgroundResource(R.drawable.bg_expansion_bar)
+            holder.binding.itemLine.visibility=View.GONE
+            holder.binding.itemCollapse.visibility=View.VISIBLE
+            holder.binding.imgEdit.visibility=View.VISIBLE
+        }
+        else {
+            holder.binding.collapsingLayout.tag=false
+            holder.binding.imgDropdown.setImageResource(R.drawable.ic_arrow_down_black)
+            holder.binding.titleLayout.setBackgroundResource(R.color.collapse_card_bg)
+            holder.binding.itemLine.visibility=View.VISIBLE
+            holder.binding.itemCollapse.visibility=View.GONE
+            holder.binding.imgEdit.visibility=View.GONE
         }
         holder.binding.itemTitleStr.text = list[position]
     }
 
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    var recyclerView: RecyclerView?=null
+    fun updateList(position: Int){
+        currentOpened = if(currentOpened == position) -1 else position
+        notifyDataSetChanged()
+        if (this.recyclerView!=null)
+            this.recyclerView?.scrollToPosition(position)
     }
 
     interface RfAnteenaItemClickListener{
