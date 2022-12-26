957f894feb7bbdec274b399c81b14a8d75049765
ankit <ankitkmaurya.iftmu@gmai.com>
2022-12-26 14:47:44 +0530

    build fixes

--- app/src/main/java/com/smarthub/baseapplication/ui/fragments/task/TaskSearchTabFragment.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/task/TaskSearchTabFragment.kt
@@ -1,19 +1,13 @@
 package com.smarthub.baseapplication.ui.fragments.task
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.FragmentSearchTaskBinding
-import com.smarthub.baseapplication.databinding.FragmentTaskBinding
-import com.smarthub.baseapplication.ui.fragments.customer_tab.powerload.PowerLoadEditDialouge
 import com.smarthub.baseapplication.ui.fragments.task.adapter.TaskAdapter
-import com.smarthub.baseapplication.ui.fragments.task.adapter.TaskAssignToDialouge
 import com.smarthub.baseapplication.viewmodels.MainViewModel
 
 class TaskSearchTabFragment : Fragment(), TaskAdapter.TaskLisListener {
