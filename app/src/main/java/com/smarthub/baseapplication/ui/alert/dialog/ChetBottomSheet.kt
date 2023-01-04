73a0e9607127f9c74ebedf73b46177449925e8e7
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-01-04 20:04:58 +0530

    chat Layout add hieght

--- app/src/main/java/com/smarthub/baseapplication/ui/alert/dialog/ChetBottomSheet.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/alert/dialog/ChetBottomSheet.kt
@@ -1,18 +1,25 @@
 package com.smarthub.baseapplication.ui.alert.dialog
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.ChatDialogBinding
+import com.smarthub.baseapplication.utils.Utils
 
 
 class ChetBottomSheet (contentLayoutId: Int): BottomSheetDialogFragment(contentLayoutId) {
     lateinit var binding : ChatDialogBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.constraint.layoutParams.height = (Utils.getScreenHeight()*0.75).toInt()
+
         binding.canecl.setOnClickListener {
             dismiss()
         }
@@ -23,4 +30,11 @@ class ChetBottomSheet (contentLayoutId: Int): BottomSheetDialogFragment(contentL
 
         return binding.root
     }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = BottomSheetDialog(requireContext(), theme)
+        dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        dialog.behavior.skipCollapsed = true
+        return dialog
+    }
+
 }
