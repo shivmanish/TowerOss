4d96b944dac636279c2120fa9803c37da9906eaa
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-01-04 23:07:26 +0530

    changes sa  agrrenmentview

--- app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/adapter/SANominalsFragmentAdapter.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/adapter/SANominalsFragmentAdapter.kt
@@ -7,6 +7,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.*
 import com.smarthub.baseapplication.ui.adapter.common.ImageAttachmentAdapter
+import com.smarthub.baseapplication.ui.fragments.opcoTenancy.BackhaulListAdapter
+import com.smarthub.baseapplication.ui.fragments.services_request.adapter.ServicesRequestAdapter
+import com.smarthub.baseapplication.ui.fragments.services_request.tableAdapters.SREquipmentTableAdapter
 
 class SANominalsFragmentAdapter(var listener: SANominalsListListener) :
     RecyclerView.Adapter<SANominalsFragmentAdapter.ViewHold>() {
@@ -28,6 +31,48 @@ class SANominalsFragmentAdapter(var listener: SANominalsListListener) :
     open class ViewHold(itemView: View) : RecyclerView.ViewHolder(itemView)
     class AgreemetViewHold(itemView: View) : ViewHold(itemView) {
         var binding: SaAgreementsItemViewBinding = SaAgreementsItemViewBinding.bind(itemView)
+
+        init {
+
+            binding.collapsingLayout.tag = false
+
+            if ((binding.collapsingLayout.tag as Boolean)) {
+                binding.imgDropdown.setImageResource(R.drawable.ic_arrow_up)
+                binding.titleLayout.setBackgroundResource(R.drawable.bg_expansion_bar)
+            } else {
+                binding.imgDropdown.setImageResource(R.drawable.ic_arrow_down_black)
+                binding.titleLayout.setBackgroundResource(R.color.collapse_card_bg)
+            }
+
+
+        }
+    }
+    class POViewHold(itemView: View) :ViewHold(itemView) {
+        var binding: EquipmentsInfoViewBinding = EquipmentsInfoViewBinding.bind(itemView)
+        var equipmentTableList: RecyclerView=binding.SrEquipmentTables
+
+        init {
+            binding.collapsingLayout.tag = false
+            if ((binding.collapsingLayout.tag as Boolean)) {
+                binding.imgDropdown.setImageResource(R.drawable.ic_arrow_up)
+                binding.titleLayout.setBackgroundResource(R.drawable.bg_expansion_bar)
+            } else {
+                binding.imgDropdown.setImageResource(R.drawable.ic_arrow_down_black)
+                binding.titleLayout.setBackgroundResource(R.color.collapse_card_bg)
+            }
+            binding.imgAdd.setOnClickListener {
+                addTableItem("dfsdh")
+            }
+        }
+        private fun addTableItem(item:String){
+            if (equipmentTableList.adapter!=null && equipmentTableList.adapter is SREquipmentTableAdapter){
+                var adapter = equipmentTableList.adapter as SREquipmentTableAdapter
+                adapter.addItem()
+            }
+        }
+    }
+    class PropertyViewHold(itemView: View) : ViewHold(itemView) {
+        var binding: PropertyDetailsListItemBinding = PropertyDetailsListItemBinding.bind(itemView)
 
         init {
 
@@ -78,6 +123,14 @@ class SANominalsFragmentAdapter(var listener: SANominalsListListener) :
                 AgreemetViewHold(view)
             }
             2 -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.property_details_list_item, parent, false)
+                PropertyViewHold(view)
+            }
+            3 -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.equipments_info_view, parent, false)
+                POViewHold(view)
+            }
+             4-> {
                 val view = LayoutInflater.from(parent.context).inflate(R.layout.sitea_greement_nominals_attachments, parent, false)
                 AttachmentViewHold(view,listener)
             }
@@ -94,6 +147,10 @@ class SANominalsFragmentAdapter(var listener: SANominalsListListener) :
             return 1
         else if (list[position] is String && list[position]==type2)
             return 2
+        else if (list[position] is String && list[position]==type3)
+            return 3
+        else if (list[position] is String && list[position]==type4)
+            return 4
         else return 0
     }
 
@@ -123,6 +180,49 @@ class SANominalsFragmentAdapter(var listener: SANominalsListListener) :
                 }
                 holder.binding.itemTitleStr.text=list[position]
             }
+            is PropertyViewHold ->{
+                holder.binding.imgEdit.setOnClickListener {
+                    listener.AgreementEditViewClick()
+                }
+                if (currentOpened == position) {
+                    holder.binding.imgDropdown.setImageResource(R.drawable.ic_arrow_up)
+                    holder.binding.titleLayout.setBackgroundResource(R.drawable.bg_expansion_bar)
+                    holder.binding.itemLine.visibility = View.GONE
+                    holder.binding.itemCollapse.visibility = View.VISIBLE
+                    holder.binding.imgEdit.visibility = View.VISIBLE
+                }
+                else {
+                    holder.binding.collapsingLayout.tag = false
+                    holder.binding.imgDropdown.setImageResource(R.drawable.ic_arrow_down_black)
+                    holder.binding.titleLayout.setBackgroundResource(R.color.collapse_card_bg)
+                    holder.binding.itemLine.visibility = View.VISIBLE
+                    holder.binding.itemCollapse.visibility = View.GONE
+                    holder.binding.imgEdit.visibility = View.GONE
+                }
+                holder.binding.collapsingLayout.setOnClickListener {
+                    updateList(position)
+                }
+                holder.binding.itemTitle.text=list[position]
+            }
+            is POViewHold->{
+                if (currentOpened == position) {
+                    holder.binding.imgDropdown.setImageResource(R.drawable.ic_arrow_up)
+                    holder.binding.titleLayout.setBackgroundResource(R.drawable.bg_expansion_bar)
+                    holder.binding.itemLine.visibility = View.GONE
+                    holder.binding.itemCollapse.visibility = View.VISIBLE
+                }
+                else {
+                    holder.binding.collapsingLayout.tag = false
+                    holder.binding.imgDropdown.setImageResource(R.drawable.ic_arrow_down_black)
+                    holder.binding.titleLayout.setBackgroundResource(R.color.collapse_card_bg)
+                    holder.binding.itemLine.visibility = View.VISIBLE
+                    holder.binding.itemCollapse.visibility = View.GONE
+                }
+                holder.binding.collapsingLayout.setOnClickListener {
+                    updateList(position)
+                }
+                holder.binding.itemTitleStr.text=list[position]
+            }
             is AttachmentViewHold->{
                 if (currentOpened == position) {
                     holder.binding.imgDropdown.setImageResource(R.drawable.ic_arrow_up)
@@ -142,12 +242,8 @@ class SANominalsFragmentAdapter(var listener: SANominalsListListener) :
                 }
                 holder.binding.itemTitleStr.text=list[position]
             }
-
-        }
-
-
-
-    }
+          }
+      }
 
     override fun getItemCount(): Int {
         return list.size
