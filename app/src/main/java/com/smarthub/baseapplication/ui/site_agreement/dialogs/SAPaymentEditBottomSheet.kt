6367a4fb4d19848fe048cb81f3347e17085e30b4
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-01-06 21:22:22 +0530

    Edit Bottom view Dialog

--- app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/dialogs/SAPaymentEditBottomSheet.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/dialogs/SAPaymentEditBottomSheet.kt
@@ -11,17 +11,18 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.CreateAlertDialogBinding
 import com.smarthub.baseapplication.databinding.SaAgreementDialogBinding
+import com.smarthub.baseapplication.databinding.SaPaymentEditDialogBinding
 import com.smarthub.baseapplication.ui.adapter.common.ImageAttachmentAdapter
 import com.smarthub.baseapplication.ui.alert.adapter.AlertImageAdapter
 import com.smarthub.baseapplication.utils.Utils
 
 
 class SAPaymentEditBottomSheet (contentLayoutId: Int): BottomSheetDialogFragment(contentLayoutId)  {
-    lateinit var binding : SaAgreementDialogBinding
+    lateinit var binding : SaPaymentEditDialogBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.constraint.layoutParams.height = (Utils.getScreenHeight()*0.75).toInt()
+      //  binding.constraint.layoutParams.height = (Utils.getScreenHeight()*0.75).toInt()
         binding.canecl.setOnClickListener {
             dismiss()
         }
@@ -29,7 +30,7 @@ class SAPaymentEditBottomSheet (contentLayoutId: Int): BottomSheetDialogFragment
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = SaAgreementDialogBinding.inflate(inflater)
+        binding = SaPaymentEditDialogBinding.inflate(inflater)
 
         return binding.root
     }
--- app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/siteagreements_tab/SAPaymentFrag.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/siteagreements_tab/SAPaymentFrag.kt
@@ -11,6 +11,9 @@ import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.PaymentFragmentBinding
 import com.smarthub.baseapplication.ui.adapter.common.ImageAttachmentAdapter
 import com.smarthub.baseapplication.ui.fragments.BaseFragment
+import com.smarthub.baseapplication.ui.site_agreement.dialogs.SAAgreementsBottomSheet
+import com.smarthub.baseapplication.ui.site_agreement.dialogs.SAPaymentEditBottomSheet
+import com.smarthub.baseapplication.ui.site_agreement.dialogs.SAPaymentViewBottomSheet
 import com.smarthub.baseapplication.ui.site_agreement.tableadapter.PaymentTableAdapter
 import com.smarthub.baseapplication.ui.site_agreement.tableadapter.SANominalsFragmentAdapter
 import com.smarthub.baseapplication.ui.site_agreement.tableadapter.SAPaymentAdapter
@@ -40,10 +43,12 @@ class SAPaymentFrag :BaseFragment(), PaymentTableAdapter.PaymentInfoListListener
     }
 
     override fun editClicked(position: Int) {
-
+        var payment = SAPaymentEditBottomSheet(R.layout.sa_payment_edit_dialog)
+        payment?.show(childFragmentManager,"category")
     }
 
     override fun viewClicked(position: Int) {
-
+        var viewPayment = SAPaymentViewBottomSheet(R.layout.sa_payment_view_dialog)
+        viewPayment?.show(childFragmentManager,"category")
     }
 }
