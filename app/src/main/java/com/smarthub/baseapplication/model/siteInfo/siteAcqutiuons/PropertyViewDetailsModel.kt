599e04097eec1d784829ce37106772da65f1ca24
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-01-07 16:28:19 +0530

    fixes siteagreement tab data

---
+++ app/src/main/java/com/smarthub/baseapplication/model/siteInfo/siteAcqutiuons/PropertyViewDetailsModel.kt
@@ -0,0 +1,12 @@
+package com.smarthub.baseapplication.model.siteInfo.siteAcqutiuons
+
+
+data class PropertyViewDetailsModel(
+    val ItemNo: String,
+    val share: String,
+    val ownerName: String,
+    val phNo: String,
+    val emailId: String,
+    val address: String,
+
+    )
