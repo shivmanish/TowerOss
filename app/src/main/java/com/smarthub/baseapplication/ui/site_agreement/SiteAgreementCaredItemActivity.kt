2b8f0b0e6579ea16d352bf354bf07c9e6a53d925
ankit <ankitkmaurya.iftmu@gmai.com>
2023-01-10 21:12:17 +0530

    crash fixes

--- app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/SiteAgreementCaredItemActivity.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/SiteAgreementCaredItemActivity.kt
@@ -22,7 +22,7 @@ class SiteAgreementCaredItemActivity : BaseActivity() {
     }
 
     lateinit var binding: ActivityNewSiteAcquisitionBinding
-    lateinit var viewmodel: HomeViewModel
+//    lateinit var viewmodel: HomeViewModel
     lateinit var adapter: SiteAgreemetAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,45 +35,40 @@ class SiteAgreementCaredItemActivity : BaseActivity() {
         binding.addMore.setOnClickListener {
             val dalouge = CommonBottomSheetDialog(R.layout.add_more_botom_sheet_dailog)
             dalouge.show(supportFragmentManager, "")
-
-            if (viewmodel.siteAgreementModel?.hasActiveObservers() == true) {
-                viewmodel.siteAgreementModel?.removeObservers(this)
-            }
-            viewmodel.siteAgreementModel?.observe(this) {
-                if (it != null && it.status == Resource.Status.LOADING) {
-                    return@observe
-                }
-                if (it?.data != null && it.status == Resource.Status.SUCCESS) {
-                    AppLogger.log("planDesign Fragment card Data fetched successfully")
-                    adapter.updateData(
-                        it.data.item!![0].Siteacquisition.get(
-                            SiteAgreementCaredItemActivity.index!!
-                        )
-                    )
-                    AppLogger.log("size :${it.data.item?.size}")
-                } else if (it != null) {
-                    Toast.makeText(
-                        this,
-                        "planDesign Fragment error :${it.message}, data : ${it.data}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    AppLogger.log("planDesign Fragment error :${it.message}, data : ${it.data}")
-                } else {
-                    AppLogger.log("planDesign Fragment Something went wrong")
-                    Toast.makeText(
-                        this,
-                        "planDesign Fragment Something went wrong",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-/*
-            binding.swipeLayout.setOnRefreshListener {
-                binding.swipeLayout.isRefreshing=false
-                viewmodel?.planAndDesignRequestAll(PowerDesignDetailsActivity.Id)
-            }*/
-
         }
+
+//        if (viewmodel.siteAgreementModel?.hasActiveObservers() == true) {
+//            viewmodel.siteAgreementModel?.removeObservers(this)
+//        }
+//        viewmodel.siteAgreementModel?.observe(this) {
+//            if (it != null && it.status == Resource.Status.LOADING) {
+//                return@observe
+//            }
+//            if (it?.data != null && it.status == Resource.Status.SUCCESS) {
+//                AppLogger.log("planDesign Fragment card Data fetched successfully")
+//                adapter.updateData(
+//                    it.data.item!![0].Siteacquisition.get(
+//                        SiteAgreementCaredItemActivity.index!!
+//                    )
+//                )
+//                AppLogger.log("size :${it.data.item?.size}")
+//            } else if (it != null) {
+//                Toast.makeText(
+//                    this,
+//                    "planDesign Fragment error :${it.message}, data : ${it.data}",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//                AppLogger.log("planDesign Fragment error :${it.message}, data : ${it.data}")
+//            } else {
+//                AppLogger.log("planDesign Fragment Something went wrong")
+//                Toast.makeText(
+//                    this,
+//                    "planDesign Fragment Something went wrong",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//        }
+
     }
 
 
@@ -89,9 +84,9 @@ class SiteAgreementCaredItemActivity : BaseActivity() {
     }
 
     override fun onDestroy() {
-        if (viewmodel.siteAgreementModel?.hasActiveObservers() == true) {
-            viewmodel.siteAgreementModel?.removeObservers(this)
-        }
+//        if (viewmodel.siteAgreementModel?.hasActiveObservers() == true) {
+//            viewmodel.siteAgreementModel?.removeObservers(this)
+//        }
         super.onDestroy()
     }
 
