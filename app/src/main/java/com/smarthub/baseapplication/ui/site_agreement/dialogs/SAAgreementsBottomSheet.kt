4d1cce23918358681f42cda35e608d092b017725
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-01-13 17:21:28 +0530

    working update data

--- app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/dialogs/SAAgreementsBottomSheet.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/dialogs/SAAgreementsBottomSheet.kt
@@ -1,6 +1,7 @@
 package com.smarthub.baseapplication.ui.site_agreement.dialogs
 
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,14 +12,17 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.SaAgreementDialogBinding
 import com.smarthub.baseapplication.helpers.AppPreferences
+import com.smarthub.baseapplication.helpers.Resource
 import com.smarthub.baseapplication.model.siteInfo.siteAgreements.SiteacquisitionAgreement
+import com.smarthub.baseapplication.utils.AppLogger
 import com.smarthub.baseapplication.utils.DropDowns
 import com.smarthub.baseapplication.utils.Utils
+import com.smarthub.baseapplication.viewmodels.HomeViewModel
 
 
 class SAAgreementsBottomSheet(
     contentLayoutId: Int,
-    var siteAgreementsData: SiteacquisitionAgreement?,
+    var siteAgreementsData: SiteacquisitionAgreement?,var viewModel: HomeViewModel
 
     ): BottomSheetDialogFragment(contentLayoutId)  {
     lateinit var binding : SaAgreementDialogBinding
@@ -30,8 +34,68 @@ class SAAgreementsBottomSheet(
         binding.canecl.setOnClickListener {
             dismiss()
         }
-     binding.textSave.setOnClickListener {
-            dismiss()
+
+        if (viewModel.basicinfoModel?.hasActiveObservers() == true){
+            viewModel.basicinfoModel?.removeObservers(viewLifecycleOwner)
+        }
+        viewModel.basicinfoModel?.observe(viewLifecycleOwner) {
+            dialog!!.dismiss()
+            dialog!!.cancel()
+        }
+        binding.textSave.setOnClickListener {
+            siteAgreementsData.let{
+
+                it?.AgreementType=   binding.editTermLease.text.toString()
+                it?.RegistrationDate=   binding.textRegistrationDate.text.toString()
+
+                binding.editAgreemenPeriod.setText(siteAgreementsData?.AgreementPeriod)
+                binding.editLockPeriod.setText(siteAgreementsData?.LockInPeriod)
+                binding.editAgreemenDate.setText(siteAgreementsData?.AgreementEffectiveDate)
+                binding.editAgreementExpiryDate.setText(siteAgreementsData?.AgreementExpiryDate)
+                binding.editRentStartDate.setText(siteAgreementsData?.RentStartDate)
+                binding.editInitialAnnualRentAmount.setText(siteAgreementsData?.InitialAnnualRentAmount)
+                binding.editPeriodicRent.setText(siteAgreementsData?.PeriodicRentPayableAmount)
+                binding.editRentEscalation.setText(siteAgreementsData?.RentEscalation)
+                binding.editRentEscalationPeriod.setText(siteAgreementsData?.RentEscalationPeriod)
+                binding.editLastEscalationDate.setText(siteAgreementsData?.LastescalationDate)
+                binding.editLastRevisedRentAmount.setText(siteAgreementsData?.LastRevisedRentAmount)
+                binding.editLastRevisedRentAmount.setText(siteAgreementsData?.LastRevisedRentAmount)
+                binding.textEBBillingBasis.setText(siteAgreementsData?.EBBillingBasis)
+                binding.editEBperunitRate.setText(siteAgreementsData?.EBPerUnitRate)
+                binding.editOnetimeAmount.setText(siteAgreementsData?.OnetimeAmount)
+                binding.editAmount.setText(siteAgreementsData?.SecurityDepositAmount)
+                binding.editArea.setText(siteAgreementsData?.GroundUsableArea)
+                binding.editGroundArea.setText(siteAgreementsData?.GroundUsableArea)
+                binding.editArea.setText(siteAgreementsData?.RooftopacquiredArea)
+
+
+            }
+       /*     siteAgreementsData?.Si = basicinfodata
+            siteAgreementsData?.id = id
+            viewModel.updateBasicInfo(basicinfoModel!!)*/
+        }
+
+
+        hideProgressLayout()
+        if (viewModel.basicInfoUpdate?.hasActiveObservers() == true)
+            viewModel.basicInfoUpdate?.removeObservers(viewLifecycleOwner)
+         viewModel.basicInfoUpdate?.observe(viewLifecycleOwner){
+            if (it!=null){
+                if (it.status == Resource.Status.LOADING){
+                    showProgressLayout()
+                }else{
+                    hideProgressLayout()
+                }
+                if (it.status == Resource.Status.SUCCESS && it.data?.Status?.isNotEmpty() == true){
+                    AppLogger.log("Successfully updated all fields")
+                    dismiss()
+                //    viewModel.fetchSiteInfoData(id)
+                }else{
+                    AppLogger.log("UnExpected Error found")
+                }
+            }else{
+                AppLogger.log("Something went wrong")
+            }
         }
 
         AppPreferences.getInstance().setDropDown(
@@ -87,6 +151,21 @@ class SAAgreementsBottomSheet(
         binding.editArea.setText(siteAgreementsData?.RooftopacquiredArea)
 
     }
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        if (viewModel.basicinfoModel?.hasActiveObservers() == true)
+            viewModel.basicinfoModel?.removeObservers(viewLifecycleOwner)
+    }
+
+    fun showProgressLayout(){
+        if (binding.progressLayout.visibility != View.VISIBLE)
+            binding.progressLayout.visibility = View.VISIBLE
+    }
+    fun hideProgressLayout(){
+        if (binding.progressLayout.visibility == View.VISIBLE)
+            binding.progressLayout.visibility = View.GONE
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = SaAgreementDialogBinding.inflate(inflater)
--- app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/siteagreements_tab/SANomonalsFrag.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/siteagreements_tab/SANomonalsFrag.kt
@@ -14,11 +14,13 @@ import com.smarthub.baseapplication.ui.site_agreement.dialogs.SAAgreementsBottom
 import com.smarthub.baseapplication.ui.site_agreement.dialogs.SAPOEditBottomSheet
 import com.smarthub.baseapplication.ui.site_agreement.dialogs.SAPOViewBottomSheet
 import com.smarthub.baseapplication.ui.site_agreement.tableadapter.PoTableAdapter
+import com.smarthub.baseapplication.viewmodels.HomeViewModel
 
 
 class SANomonalsFrag( val siteacquisitionAgreements: List<SiteacquisitionAgreement>?) : BaseFragment(), PoTableAdapter.PoInfoListListener {
     lateinit var adapter : SANominalsFragmentAdapter
     var binding : NominalsFragmentBinding?=null
+    lateinit var homeViewModel: HomeViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = NominalsFragmentBinding.inflate(inflater, container, false)
         return binding?.root
@@ -46,7 +48,7 @@ class SANomonalsFrag( val siteacquisitionAgreements: List<SiteacquisitionAgreeme
     }
 
     override fun AgreementEditViewClick(siteAgreementsData: SiteacquisitionAgreement?) {
-        var saAgreementsBottomSheet = SAAgreementsBottomSheet(R.layout.sa_agreement_dialog,siteAgreementsData)
+        var saAgreementsBottomSheet = SAAgreementsBottomSheet(R.layout.sa_agreement_dialog,siteAgreementsData,homeViewModel)
         saAgreementsBottomSheet?.show(childFragmentManager,"category")
     }
 
