7bf27832b164ec3f05bcbb8665cb4933d0c150c7
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-01-14 17:10:23 +0530

    po details view and edoit dialog

--- app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/dialogs/SAProprttyOwnerEditBottomSheet.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/site_agreement/dialogs/SAProprttyOwnerEditBottomSheet.kt
@@ -9,27 +9,28 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.smarthub.baseapplication.R
-import com.smarthub.baseapplication.databinding.CreateAlertDialogBinding
-import com.smarthub.baseapplication.databinding.SaAgreementDialogBinding
 import com.smarthub.baseapplication.databinding.SaOwnerPropertyEditDialogBinding
-import com.smarthub.baseapplication.ui.adapter.common.ImageAttachmentAdapter
-import com.smarthub.baseapplication.ui.alert.adapter.AlertImageAdapter
 import com.smarthub.baseapplication.utils.Utils
 
 
-class SAProprttyOwnerEditBottomSheet (contentLayoutId: Int): BottomSheetDialogFragment(contentLayoutId)  {
-    lateinit var binding : SaOwnerPropertyEditDialogBinding
+class SAProprttyOwnerEditBottomSheet(contentLayoutId: Int) :
+    BottomSheetDialogFragment(contentLayoutId) {
+    lateinit var binding: SaOwnerPropertyEditDialogBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-     binding.constraint.layoutParams.height = (Utils.getScreenHeight()*0.75).toInt()
+        binding.constraint.layoutParams.height = (Utils.getScreenHeight() * 0.75).toInt()
         binding.canecl.setOnClickListener {
             dismiss()
         }
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = SaOwnerPropertyEditDialogBinding.inflate(inflater)
 
         return binding.root
@@ -41,7 +42,6 @@ class SAProprttyOwnerEditBottomSheet (contentLayoutId: Int): BottomSheetDialogFr
         dialog.behavior.skipCollapsed = true
         return dialog
     }
-
 
 
     override fun getTheme() = R.style.NewDialogTask
