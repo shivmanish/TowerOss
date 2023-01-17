e5cafd4cac99febb56dffe1b39b61a1085da4ad1
manis <manish.sharma@smartmile.com>
2023-01-17 19:29:54 +0530

    ui update in task fragment

--- app/src/main/java/com/smarthub/baseapplication/ui/fragments/home/MyTaskFragment.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/home/MyTaskFragment.kt
@@ -41,7 +41,6 @@ class MyTaskFragment(var listener: TaskListener) : Fragment() {
                 list.addAll(it)
                 adapterList.updateList(list)
             }else{
-//                no data found
                 AppLogger.log("myTask data is null")
                 adapterList.addItem("no_data")
             }
