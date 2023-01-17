d2223ed08b3e2b760056b1dac20118b598438973
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-01-17 15:07:38 +0530

    Add Details

--- app/src/main/java/com/smarthub/baseapplication/ui/fragments/noc/bottomSheetAdapters/AuthorityDetailsDialougeAdapter.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/noc/bottomSheetAdapters/AuthorityDetailsDialougeAdapter.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.NocAuthorityDetailsDialougeLayoutBinding
+import com.smarthub.baseapplication.helpers.AppPreferences
 import com.smarthub.baseapplication.model.siteInfo.nocAndCompModel.AuthorityDetails
+import com.smarthub.baseapplication.utils.DropDowns
 
 class AuthorityDetailsDialougeAdapter(contentLayoutId: Int,var authorityDetails: AuthorityDetails?): BottomSheetDialogFragment(contentLayoutId) {
     lateinit var binding : NocAuthorityDetailsDialougeLayoutBinding
@@ -17,6 +19,23 @@ class AuthorityDetailsDialougeAdapter(contentLayoutId: Int,var authorityDetails:
         binding.canecl.setOnClickListener {
             dismiss()
         }
+
+        AppPreferences.getInstance().setDropDown(
+            binding.authorityPrefferedLanguage,
+            DropDowns.ApplicationInitialPreferredLaungauge.name,
+            authorityDetails?.ApplicationInitialPreferredLaungauge
+        )
+
+
+
+
+        binding.authorityName.setText(authorityDetails?.Name)
+        binding.authorityContactPerson.setText(authorityDetails?.ContactPerson)
+        binding.authorityContactPerson.setText(authorityDetails?.ContactPerson)
+        binding.authorityContactNumber.setText(authorityDetails?.ContactNumber)
+        binding.authorityEmail.setText(authorityDetails?.EmailId)
+        binding.authorityLandmark.setText(authorityDetails?.LandMark)
+
     }
 
     override fun getTheme() = R.style.NewDialogTask
--- app/src/main/java/com/smarthub/baseapplication/utils/DropDowns.java
+++ app/src/main/java/com/smarthub/baseapplication/utils/DropDowns.java
@@ -18,5 +18,6 @@ public enum DropDowns {
     ApplicationInitialCategory,
     ApplicationInitialApplicationType,
     ApplicationInitialApplicationStatus,
+    ApplicationInitialPreferredLaungauge,
 
 }
