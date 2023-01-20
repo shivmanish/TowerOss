f7d12d9049b8af368bbfea5bda631b45be4b8d18
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-01-20 21:36:38 +0530

    fixes noc tab in pd

---
+++ app/src/main/java/com/smarthub/baseapplication/ui/fragments/plandesign/dialouge/PDNocComplainViewDialouge.kt
@@ -0,0 +1,46 @@
+package com.smarthub.baseapplication.ui.fragments.plandesign.dialouge
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.DialogFragment
+import com.smarthub.baseapplication.R
+import com.smarthub.baseapplication.databinding.*
+
+
+class PDNocComplainViewDialouge : DialogFragment() {
+lateinit var binding: PdNocViewDialogBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+         super.onCreateView(inflater, container, savedInstanceState)
+        binding = PdNocViewDialogBinding.inflate(inflater)
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog!!.window!!.setLayout(width, height)
+        dialog!!.window!!.setGravity(Gravity.BOTTOM)
+        dialog!!.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog!!.window!!.setWindowAnimations(R.style.DialogAnimation)
+        return binding.root
+    }
+
+    override fun getTheme(): Int {
+        return R.style.FullDialog
+    }
+
+    override fun isCancelable(): Boolean {
+        return true
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.canecl.setOnClickListener{
+            dialog!!.dismiss()
+            dialog!!.cancel()
+        }
+
+    }
+
+
+}
