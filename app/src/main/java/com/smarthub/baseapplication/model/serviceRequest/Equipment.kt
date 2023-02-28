68ca0e8ba74c87b755bd0db4b844ac76dbb28586
ankit <ankitkmaurya.iftmu@gmai.com>
2023-02-28 14:01:25 +0530

    sr equipment completed

--- app/src/main/java/com/smarthub/baseapplication/model/serviceRequest/Equipment.kt
+++ app/src/main/java/com/smarthub/baseapplication/model/serviceRequest/Equipment.kt
@@ -1,11 +1,18 @@
 package com.smarthub.baseapplication.model.serviceRequest
 
 data class Equipment(
-    var CabinetSize: String? = null,
-    var EquipmentWeight: String? = null,
-    var InputPower: String? = null,
+    var SizeL: String? = null,
+    var SizeB: String? = null,
+    var SizeH: String? = null,
+    var Weight: String? = null,
+    var InstallationType: Int,
+    var EquipmentName: List<String>,
+    var PowerType: String? = null,
     var MaxPowerRating: String? = null,
-    var OperatingTemp: String? = null,
+    var VoltageRangeMin: String? = null,
+    var VoltageRangeMax: String? = null,
+    var OperatingTempMin: String? = null,
+    var OperatingTempMax: String? = null,
     var Technology: List<Int>?=null,
     var Voltage: String? = null,
     var created_at: String? = null,
--- app/src/main/java/com/smarthub/baseapplication/ui/dialog/services_request/EditEquipmentBottomSheet.kt
+++ app/src/main/java/com/smarthub/baseapplication/ui/dialog/services_request/EditEquipmentBottomSheet.kt
@@ -9,12 +9,13 @@ import com.smarthub.baseapplication.R
 import com.smarthub.baseapplication.databinding.EditEquipementDialougeBinding
 import com.smarthub.baseapplication.helpers.AppPreferences
 import com.smarthub.baseapplication.model.serviceRequest.*
+import com.smarthub.baseapplication.ui.dialog.qat.BaseBottomSheetDialogFragment
 import com.smarthub.baseapplication.ui.dialog.siteinfo.pojo.BasicinfoModel
 import com.smarthub.baseapplication.utils.DropDowns
 import com.smarthub.baseapplication.utils.Utils
 import com.smarthub.baseapplication.viewmodels.HomeViewModel
 
-class EditEquipmentBottomSheet(contentLayoutId: Int, var equipmant: Equipment?, var serviceRequestAllData: ServiceRequestAllDataItem?, var viewmodel: HomeViewModel?, var Id: String, ) : BottomSheetDialogFragment(contentLayoutId) {
+class EditEquipmentBottomSheet(val pos:Int,var equipmant: Equipment?, var serviceRequestAllData: ServiceRequestAllDataItem?, var viewmodel: HomeViewModel?, var Id: String, ) : BaseBottomSheetDialogFragment() {
     lateinit var binding: EditEquipementDialougeBinding
     var basicinfoModel: BasicinfoModel? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -25,48 +26,49 @@ class EditEquipmentBottomSheet(contentLayoutId: Int, var equipmant: Equipment?, 
         binding.canecl.setOnClickListener {
             dismiss()
         }
-        AppPreferences.getInstance().setDropDown(binding.technology,DropDowns.SREquipmentsTechnology.name,"1")
-        AppPreferences.getInstance().setDropDown(binding.equipment,DropDowns.SREquipmentsEquipment.name,"1")
-        AppPreferences.getInstance().setDropDown(binding.type,DropDowns.SREquipmentsType.name,"1")
+
         equipmant?.let {
-            AppPreferences.getInstance().setDropDown(binding.technology,DropDowns.SREquipmentsTechnology.name,it.Technology?.get(0).toString())
+            binding.srNo.text = "$pos"
+            if (it.EquipmentName.isNotEmpty())
+                AppPreferences.getInstance().setDropDown(binding.equipmentName,DropDowns.EquipmentName.name,"${it.EquipmentName[0]}")
 
-            binding.cabinetsize.setText("NA")
-            binding.equipmentWeight.setText(it.EquipmentWeight)
-            binding.inputPower.setText(it.InputPower)
-            binding.voltage.setText(it.Voltage)
-            binding.maxPowerRating.setText(it.MaxPowerRating)
-            binding.operatingPower.setText(it.OperatingTemp)
+            binding.type.text = "${it.InstallationType}"
+            binding.size.text = "${it.SizeL}|${it.SizeB}|${it.SizeH}"
+            binding.equipmentWeight.text = "${it.Weight}"
+            binding.powerType.text = it.PowerType
+            binding.voltage.text = "${it.VoltageRangeMin}|${it.VoltageRangeMax}"
+            binding.maxPowerRating.text = it.MaxPowerRating
+            binding.operatingPower.text = "${it.OperatingTempMin}|${it.OperatingTempMax}"
         }
 
-        binding.update.setOnClickListener {
-            equipmant?.let {
-                it.Technology = ArrayList(arrayOf(binding.technology.selectedValue.id.toInt()).asList())
-
-                it.EquipmentWeight = binding.equipmentWeight.text.toString()
-                it.InputPower = binding.inputPower.text.toString()
-                it.Voltage = binding.voltage.text.toString()
-                it.MaxPowerRating = binding.maxPowerRating.text.toString()
-                it.OperatingTemp = binding.operatingPower.text.toString()
-
-                val mServiceRequestAllDataItem = ServiceRequestAllDataItem()
-                mServiceRequestAllDataItem.id = serviceRequestAllData?.id
-                mServiceRequestAllDataItem.ServiceRequest = ArrayList()
-
-                val serviceRequest = ServiceRequest()
-                serviceRequest.Equipments = ArrayList()
-                (serviceRequest.Equipments as ArrayList<Equipment>).add(it)
-                serviceRequest.id = serviceRequestAllData?.ServiceRequest!![0].id
-                mServiceRequestAllDataItem.ServiceRequest?.add(serviceRequest)
-
-                val serviceRequestList = ServiceRequestAllData()
-                serviceRequestList.add(mServiceRequestAllDataItem)
-
-                basicinfoModel?.ServiceRequestMain = serviceRequestList
-                basicinfoModel?.id = Id
-                viewmodel?.updateBasicInfo(basicinfoModel!!)
-            }
-        }
+//        binding.update.setOnClickListener {
+//            equipmant?.let {
+//                it.Technology = ArrayList(arrayOf(binding.technology.selectedValue.id.toInt()).asList())
+//
+//                it.EquipmentWeight = binding.equipmentWeight.text.toString()
+//                it.InputPower = binding.inputPower.text.toString()
+//                it.Voltage = binding.voltage.text.toString()
+//                it.MaxPowerRating = binding.maxPowerRating.text.toString()
+//                it.OperatingTemp = binding.operatingPower.text.toString()
+//
+//                val mServiceRequestAllDataItem = ServiceRequestAllDataItem()
+//                mServiceRequestAllDataItem.id = serviceRequestAllData?.id
+//                mServiceRequestAllDataItem.ServiceRequest = ArrayList()
+//
+//                val serviceRequest = ServiceRequest()
+//                serviceRequest.Equipments = ArrayList()
+//                (serviceRequest.Equipments as ArrayList<Equipment>).add(it)
+//                serviceRequest.id = serviceRequestAllData?.ServiceRequest!![0].id
+//                mServiceRequestAllDataItem.ServiceRequest?.add(serviceRequest)
+//
+//                val serviceRequestList = ServiceRequestAllData()
+//                serviceRequestList.add(mServiceRequestAllDataItem)
+//
+//                basicinfoModel?.ServiceRequestMain = serviceRequestList
+//                basicinfoModel?.id = Id
+//                viewmodel?.updateBasicInfo(basicinfoModel!!)
+//            }
+//        }
     }
 
     override fun getTheme() = R.style.NewDialogTask
