8b0cd5167f47900f154bd0c2b31f647d93b0498f
manis <manish.sharma@smartmile.com>
2023-03-04 16:13:09 +0530

    update in siteInfo tab

--- app/src/main/java/com/smarthub/baseapplication/utils/DropDowns.java
+++ app/src/main/java/com/smarthub/baseapplication/utils/DropDowns.java
@@ -39,6 +39,7 @@ public enum DropDowns {
     OverallFeasibility,
     PowerConnectionType,
     PaymentStatus,
+    Climatezone,
 
     ApplicationInitialCategory,
     ApplicationInitialApplicationType,
