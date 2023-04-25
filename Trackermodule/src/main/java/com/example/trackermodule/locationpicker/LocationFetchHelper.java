d79b910cc15a6975a5e8675a85314c69457b8e3a
somnath <somnathbca14@gmail.com>
2023-04-25 20:46:11 +0530

    location fetcher new api

--- Trackermodule/src/main/java/com/example/trackermodule/locationpicker/LocationFetchHelper.java
+++ Trackermodule/src/main/java/com/example/trackermodule/locationpicker/LocationFetchHelper.java
@@ -8,23 +8,29 @@ import android.location.LocationManager;
 
 import androidx.core.app.ActivityCompat;
 
-public class LocationFetchHelper {
+public class LocationFetchHelper implements LocationFetchCallback {
     LocationManager locationManager;
     LocationListener locationListener;
+    LocationFetchCallback locationFetchCallback;
     public LocationFetchHelper(Context context, LocationFetchCallback locationFetchCallback) {
         locationManager = (LocationManager)
                 context.getSystemService(Context.LOCATION_SERVICE);
-
-         locationListener = new MyLocationListener(context,locationFetchCallback);
+        this.locationFetchCallback = locationFetchCallback;
+         locationListener = new MyLocationListener(context,this);
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return ;
         }
         locationManager.requestLocationUpdates(
                 LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
     }
-    public void stoplocation(){
+
+    @Override
+    public void OnLocationFetched(AddresData addresData) {
         if(locationManager!=null && locationListener!=null ) {
             locationManager.removeUpdates(locationListener);
+        }
+        if(this.locationFetchCallback!=null){
+            this.locationFetchCallback.OnLocationFetched(addresData);
         }
     }
 }
