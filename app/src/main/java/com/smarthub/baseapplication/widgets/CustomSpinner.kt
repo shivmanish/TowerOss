1231158dd92e1c7baf9ce0ddc6ff20e97c724ae9
somnath <somnathbca14@gmail.com>
2023-05-02 23:11:48 +0530

    Opco Crash issue

--- app/src/main/java/com/smarthub/baseapplication/widgets/CustomSpinner.kt
+++ app/src/main/java/com/smarthub/baseapplication/widgets/CustomSpinner.kt
@@ -9,12 +9,9 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatSpinner
-import androidx.lifecycle.ViewModelProvider
 import com.smarthub.baseapplication.model.dropdown.DropDownItem
 import com.smarthub.baseapplication.ui.adapter.spinner.CustomArrayAdapter
-import com.smarthub.baseapplication.ui.alert.viewmodel.AlertViewModel
 import com.smarthub.baseapplication.utils.AppLogger
-import com.smarthub.baseapplication.viewmodels.HomeViewModel
 import org.w3c.dom.Text
 
 class CustomSpinner : AppCompatSpinner {
@@ -28,6 +25,13 @@ class CustomSpinner : AppCompatSpinner {
 
     fun setOnItemSelectionListener(listener: ItemSelectedListener){
         itemSelectedListener = listener
+    }
+
+    fun getSelectedArray():ArrayList<Int>{
+        val arr = ArrayList<Int>()
+        if (selectedValue.id!="-1")
+            arr.add(selectedValue.id.toInt())
+        return arr
     }
 
     fun init() {
@@ -116,22 +120,6 @@ class CustomSpinner : AppCompatSpinner {
         adapter = CustomArrayAdapter(context, data)
         setSelection(getIndexById(id))
         var listener = object : OnItemSelectedListener{
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                onItemSelected(data[position])
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-        }
-        onItemSelectedListener = listener
-    }
-    fun setDepartmenmtSpinnerData(data: List<DropDownItem>, id: String?) {
-        this.data = data
-        adapter = CustomArrayAdapter(context, data)
-        setSelection(getIndexById(id))
-        val listener = object : OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 onItemSelected(data[position])
             }
