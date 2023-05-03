4d8471caddacdce50bd868475924c3eb1424c690
manis <manish.sharma@smartmile.com>
2023-05-03 19:08:24 +0530

    towerCivilInfra api and model class updated

---
+++ app/src/main/java/com/smarthub/baseapplication/model/siteIBoard/newTowerCivilInfra/TowerCubeTesting.kt
@@ -0,0 +1,19 @@
+package com.smarthub.baseapplication.model.siteIBoard.newTowerCivilInfra
+
+data class TowerCubeTesting(
+    val ActualCompletionDate: String,
+    val StartDAte: String,
+    val TAT: Int,
+    val VendorEmailId: String,
+    val VendorExecutiveName: String,
+    val VendorExecutiveNumber: String,
+    val attachment: List<Any>,
+    val created_at: String,
+    val created_by: Int,
+    val id: Int,
+    val isActive: Boolean,
+    val licenseeCompany: Int,
+    val modified_at: String,
+    val modified_by: Int,
+    val remark: String
+)
